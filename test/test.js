ffe876e1ebcf36117870d31dbf91f45c971f0e90
Kevin Martensson <kevinmartensson@gmail.com>
2014-11-25 15:51:53 +0100

    Dogfood tests

--- test/test.js
+++ test/test.js
@@ -1,103 +1,98 @@
 'use strict';
-var assert = require('assert');
-// TODO: dogfood when ava is stable?
-var test = require('tape');
-var test2 = require('../lib/test');
-
-// TODO: lots of more tests
+var test = require('../');
+var ava = require('../lib/test');
 
 test('run test', function (t) {
-	test2('foo', function (t2) {
-		t2.true(false);
-		t2.end();
+	ava('foo', function (a) {
+		a.true(false);
+		a.end();
 	}).run(function (err) {
 		t.true(err);
 		t.end();
 	});
 });
 
-test('optional test title', function (t) {
-	test2(function (t2) {
-		t2.end();
+test('test title is optional', function (t) {
+	ava(function (a) {
+		a.end();
 	}).run(function () {
-		t.equal(this.title, '[anonymous]');
+		t.is(this.title, '[anonymous]');
 		t.end();
 	});
 });
 
 test('infer test name from function', function (t) {
-	test2(function foo(t2) {
-		t2.end();
+	ava(function foo(a) {
+		a.end();
 	}).run(function () {
-		t.equal(this.title, 'foo');
+		t.is(this.title, 'foo');
 		t.end();
 	});
 });
 
 test('multiple asserts', function (t) {
-	test2(function (t2) {
-		t2.true(true);
-		t2.true(false);
-		t2.true(true);
-		t2.end();
-	}).run(function (err) {
-		t.true(err);
+	ava(function (a) {
+		a.true(true);
+		a.true(true);
+		a.true(true);
+		a.end();
+	}).run(function () {
+		t.is(this._assertCount, 3);
 		t.end();
 	});
 });
 
 test('plan assertions', function (t) {
-	test2(function (t2) {
-		t2.plan(2)
-		t2.true(true);
-		t2.true(true);
+	ava(function (a) {
+		a.plan(2);
+		a.true(true);
+		a.true(true);
+	}).run(function () {
+		t.is(this._planCount, 2);
+		t.is(this._assertCount, 2);
+		t.end();
+	});
+});
+
+test('run more assertions than planned', function (t) {
+	ava(function (a) {
+		a.plan(2);
+		a.true(true);
+		a.true(true);
+		a.true(true);
+	}).run(function (err) {
+		t.true(err);
+		t.is(err.name, 'AssertionError');
+		t.end();
+	});
+});
+
+test('handle non-assertion errors', function (t) {
+	ava(function () {
+		throw new Error();
+	}).run(function (err) {
+		t.is(err.name, 'AssertionError');
+		t.true(err.actual instanceof Error);
+		t.end();
+	});
+});
+
+test('handle testing of arrays', function (t) {
+	ava(function (a) {
+		a.same(['foo', 'bar'], ['foo', 'bar']);
+		a.end();
 	}).run(function (err) {
 		t.false(err);
 		t.end();
 	});
 });
 
-test('plan assertions 2', function (t) {
-	test2(function (t2) {
-		t2.plan(2)
-		t2.true(true);
-		t2.true(true);
+test('handle testing of objects', function (t) {
+	ava(function (a) {
+		a.same({foo: 'foo', bar: 'bar'}, {foo: 'foo', bar: 'bar'});
+		a.end();
 	}).run(function (err) {
-		t.notOk(err);
-		t.end();
-	});
-});
-
-test('plan assertions 3', function (t) {
-	test2(function (t2) {
-		t2.plan(2)
-		t2.true(false);
-		t2.true(false);
-	}).run(function (err) {
-		t.true(err);
-		t.end();
-	});
-});
-
-test('plan assertions - more than planned', function (t) {
-	test2(function foo(t2) {
-		t2.plan(2)
-		t2.true(false);
-		t2.true(false);
-		t2.true(false);
-	}).run(function (err) {
-		t.equal(err.name, 'AssertionError');
-		t.end();
-	});
-});
-
-test('handle non-assertion errors', function (t) {
-	test2(function (t2) {
-		throw new Error;
-		t2.end();
-	}).run(function (err) {
-		t.equal(err.name, 'AssertionError');
-		t.true(err.actual instanceof Error);
+		t.false(err);
 		t.end();
 	});
 });
