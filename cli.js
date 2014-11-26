08cf39818b8524b5ef5c6567448aed22e10027a5
Kevin Martensson <kevinmartensson@gmail.com>
2014-11-26 20:09:44 +0100

    Add support for globbing in CLI

--- cli.js
+++ cli.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 'use strict';
-var updateNotifier = require('update-notifier');
+var path = require('path');
+var globby = require('globby');
 var meow = require('meow');
-var ava = require('./');
+var updateNotifier = require('update-notifier');
 
 var cli = meow({
 	help: [
@@ -21,5 +22,18 @@ updateNotifier({
 	packageVersion: cli.pkg.version
 }).notify();
 
-console.error('not implemented');
-process.exit(1);
+if (cli.input.length === 0) {
+	console.error('Input required');
+	process.exit(1);
+}
+
+globby(cli.input, function (err, files) {
+	if (err) {
+		console.error(err.message);
+		process.exit(1);
+	}
+
+	files.forEach(function (file) {
+		require(path.resolve(process.cwd(), file));
+	});
+});
