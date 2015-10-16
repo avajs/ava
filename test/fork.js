927a0104941743a73daa3fddba56d2e1643a03aa
Vsevolod Strukchinsky <floatdrop@gmail.com>
2015-10-16 22:08:29 +0500

    add some tests on fork.js

---
+++ test/fork.js
@@ -0,0 +1,41 @@
+'use strict';
+var test = require('tape');
+var join = require('path').join;
+var fork = require('../lib/fork.js');
+
+function fixture(name) {
+	return join(__dirname, 'fixture', name);
+}
+
+test('emits test event', function (t) {
+	fork(fixture('generators.js'))
+		.on('test', function (tt) {
+			t.equal(tt.title, 'generator function');
+			t.end();
+		});
+});
+
+test('resolves promise with tests info', function (t) {
+	var file = fixture('generators.js');
+
+	fork(file)
+		.then(function (info) {
+			t.equal(info.stats.passCount, 1);
+			t.equal(info.tests.length, 1);
+			t.equal(info.file, file);
+			t.end();
+		});
+});
+
+test('rejects on error and streams output', function (t) {
+	var buffer = '';
+
+	fork(fixture('broken.js'))
+		.on('data', function (data) {
+			buffer += data;
+		})
+		.catch(function () {
+			t.ok(/Cannot find module/.test(buffer));
+			t.end();
+		});
+});
