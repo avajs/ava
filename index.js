c84c48a8c3e2ac14f8f0bf4473de8b78688aea6a
vdemedes <vdemedes@gmail.com>
2015-10-24 16:22:08 +0200

    support running the test file only via cli

--- index.js
+++ index.js
@@ -2,10 +2,8 @@
 var setImmediate = require('set-immediate-shim');
 var Runner = require('./lib/runner');
 var runner = new Runner();
-var log = require('./lib/logger');
 
 var isFailFast = process.argv.indexOf('--fail-fast') !== -1;
-var isForked = process.env.AVA_FORK;
 
 // if fail-fast is enabled, use this variable to detect,
 // that no more tests should be logged
@@ -27,70 +25,43 @@ function test(err, title, duration) {
 		return;
 	}
 
-	if (isForked) {
-		if (err) {
-			err = serializeError(err);
-		}
-
-		process.send({
-			name: 'test',
-			data: {
-				err: err || {},
-				title: title,
-				duration: duration
-			}
-		});
-
-		if (err && isFailFast) {
-			isFailed = true;
-			exit();
-		}
-
-		return;
+	if (err) {
+		err = serializeError(err);
 	}
 
-	log.test(err, title, duration);
+	process.send({
+		name: 'test',
+		data: {
+			err: err || {},
+			title: title,
+			duration: duration
+		}
+	});
+
+	if (err && isFailFast) {
+		isFailed = true;
+		exit();
+	}
 }
 
 function exit() {
-	if (isForked) {
-		// serialize errors
-		runner.results.forEach(function (result) {
-			if (result.error) {
-				result.error = serializeError(result.error);
-			}
-		});
+	// serialize errors
+	runner.results.forEach(function (result) {
+		if (result.error) {
+			result.error = serializeError(result.error);
+		}
+	});
 
-		process.send({
-			name: 'results',
-			data: {
-				stats: runner.stats,
-				tests: runner.results
-			}
-		});
-
-		return;
-	}
-
-	var stats = runner.stats;
-	var results = runner.results;
-
-	log.write();
-	log.report(stats.passCount, stats.failCount);
-	log.write();
-
-	if (stats.failCount > 0) {
-		log.errors(results);
-	}
-
-	process.exit(stats.failCount > 0 ? 1 : 0);
+	process.send({
+		name: 'results',
+		data: {
+			stats: runner.stats,
+			tests: runner.results
+		}
+	});
 }
 
 setImmediate(function () {
-	if (!isForked) {
-		log.write();
-	}
-
 	runner.on('test', test);
 	runner.run().then(exit);
 });
