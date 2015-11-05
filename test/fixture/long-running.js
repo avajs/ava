5c1749682f0b9d963e66ab8aeee921b579a43fae
James Talmage <james@talmage.io>
2015-11-05 17:01:20 -0500

    add failing test demonstrating long running onExit() hook in child process

--- test/fixture/long-running.js
+++ test/fixture/long-running.js
@@ -1,12 +1,25 @@
 'use strict';
 const test = require('../../');
+var onExit = require('signal-exit');
 
 test('long running', function (t) {
 	t.plan(1);
 
+	onExit(function () {
+		// simulate an exit hook that lasts a short while
+		var start = Date.now();
+		while(Date.now() - start < 2000) {
+			//synchronously wait for 2 seconds
+		}
+		process.send({name:'cleanup-completed', data: {completed: true}});
+	}, {alwaysLast: true});
+
 	setTimeout(function () {
+		t.ok(true);
+	});
+
+	setTimeout(function () {
+		// this would keep the process running for a long time.
 		console.log('I\'m gonna live forever!!');
 	}, 15000);
-
-	t.ok(true);
 });
