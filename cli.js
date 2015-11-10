6a8f0e7a7caf833627c4645e74f8632e1c0c02b8
James Talmage <james@talmage.io>
2015-11-10 14:16:32 +0700

    Close #169 PR: Fix Windows onExit failure..

--- cli.js
+++ cli.js
@@ -158,7 +158,10 @@ function exit(results) {
 	// correctly flush the output when multiple test files
 	process.stdout.write('');
 
-	process.exit(failed > 0 ? 1 : 0);
+	// timeout required to correctly flush stderr on Node 0.10 Windows
+	setTimeout(function () {
+		process.exit(failed > 0 ? 1 : 0);
+	}, 0);
 }
 
 function init(files) {
