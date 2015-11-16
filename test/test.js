05bee73b520d740aadc89e6bb1724aceac7e23ac
Jonathan Haines <jonno.haines@gmail.com>
2015-11-16 18:53:55 +0700

    Close #165 PR: Support Observable. Fixes #84

--- test/test.js
+++ test/test.js
@@ -7,6 +7,7 @@ var test = require('tape');
 var Runner = require('../lib/runner');
 var fork = require('../lib/fork');
 var ava = require('../lib/test');
+var Observable = require('./fixture/observable');
 
 function execCli(args, cb) {
 	if (!Array.isArray(args)) {
@@ -90,6 +91,25 @@ test('plan assertions with support for promises', function (t) {
 		}, 200);
 
 		return promise;
+	}).run().then(function (a) {
+		t.is(a.planCount, 2);
+		t.is(a.assertCount, 2);
+		t.end();
+	});
+});
+
+test('plan assertions with support for observables', function (t) {
+	ava(function (a) {
+		a.plan(2);
+
+		var observable = Observable.of();
+
+		setTimeout(function () {
+			a.pass();
+			a.pass();
+		}, 200);
+
+		return observable;
 	}).run().then(function (a) {
 		t.is(a.planCount, 2);
 		t.is(a.assertCount, 2);
@@ -276,6 +296,94 @@ test('handle throws with false-positive promise', function (t) {
 	});
 });
 
+test('handle throws with thrown observable', function (t) {
+	ava(function (a) {
+		a.plan(1);
+
+		var observable = new Observable(function (observer) {
+			observer.error(new Error());
+		});
+		a.throws(observable);
+	}).run().then(function (a) {
+		t.false(a.assertError);
+		t.end();
+	});
+});
+
+test('handle throws with long running thrown observable', function (t) {
+	ava(function (a) {
+		a.plan(1);
+
+		var observable = new Observable(function (observer) {
+			setTimeout(function () {
+				observer.error(new Error('abc'));
+			}, 2000);
+		});
+
+		a.throws(observable, /abc/);
+	}).run().then(function (a) {
+		t.false(a.assertError);
+		t.end();
+	});
+});
+
+test('handle throws with completed observable', function (t) {
+	ava(function (a) {
+		a.plan(1);
+
+		var observable = Observable.of();
+		a.throws(observable);
+	}).run().catch(function (err) {
+		t.true(err);
+		t.is(err.name, 'AssertionError');
+		t.end();
+	});
+});
+
+test('handle throws with regex', function (t) {
+	ava(function (a) {
+		a.plan(1);
+
+		var observable = new Observable(function (observer) {
+			observer.error(new Error('abc'));
+		});
+		a.throws(observable, /abc/);
+	}).run().then(function (a) {
+		t.false(a.assertionError);
+		t.end();
+	});
+});
+
+test('handle throws with string', function (t) {
+	ava(function (a) {
+		a.plan(1);
+
+		var observable = new Observable(function (observer) {
+			observer.error(new Error('abc'));
+		});
+		a.throws(observable, 'abc');
+	}).run().then(function (a) {
+		t.false(a.assertionError);
+		t.end();
+	});
+});
+
+test('handle throws with false-positive observable', function (t) {
+	ava(function (a) {
+		a.plan(1);
+
+		var observable = new Observable(function (observer) {
+			observer.next(new Error());
+			observer.complete();
+		});
+		a.throws(observable);
+	}).run().catch(function (err) {
+		t.true(err);
+		t.is(err.name, 'AssertionError');
+		t.end();
+	});
+});
+
 test('handle doesNotThrow with error', function (t) {
 	ava(function (a) {
 		a.doesNotThrow(function () {
@@ -321,6 +429,33 @@ test('handle doesNotThrow with rejected promise', function (t) {
 
 		var promise = Promise.reject(new Error());
 		a.doesNotThrow(promise);
+	}).run().catch(function (err) {
+		t.true(err);
+		t.is(err.name, 'AssertionError');
+		t.end();
+	});
+});
+
+test('handle doesNotThrow with completed observable', function (t) {
+	ava(function (a) {
+		a.plan(1);
+
+		var observable = Observable.of();
+		a.doesNotThrow(observable);
+	}).run().then(function (a) {
+		t.false(a.assertError);
+		t.end();
+	});
+});
+
+test('handle doesNotThrow with thrown observable', function (t) {
+	ava(function (a) {
+		a.plan(1);
+
+		var observable = new Observable(function (observer) {
+			observer.error(new Error());
+		});
+		a.doesNotThrow(observable);
 	}).run().catch(function (err) {
 		t.true(err);
 		t.is(err.name, 'AssertionError');
