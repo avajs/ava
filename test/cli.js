7280d9d15c26c0a651ae453328b220b58bb901ca
James McMath <mcmathja@gmail.com>
2015-12-03 23:37:34 -0500

    test for filename prefix on stack traces

--- test/cli.js
+++ test/cli.js
@@ -89,6 +89,16 @@ test('display test title prefixes', function (t) {
 	});
 });
 
+test('display filename prefixes for failed test stack traces', function (t) {
+	t.plan(2);
+
+	execCli(['fixture/es2015.js', 'fixture/one-pass-one-fail.js'], function (err, stdout, stderr) {
+		t.ok(err);
+		t.match(stderr, /1. one-pass-one-fail › this is a failing test/);
+		t.end();
+	});
+});
+
 test('don\'t display test title if there is only one anonymous test', function (t) {
 	t.plan(2);
 
