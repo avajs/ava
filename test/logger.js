407a5cb27dbe6a294366e9011959cd54b20e4779
LuKas Corso <carnubak@carnubak.com>
2016-02-17 22:16:55 -0300

    Adds method checks for the rest of Logger methods

--- test/logger.js
+++ test/logger.js
@@ -29,3 +29,87 @@ test('only write if start is supported by reporter', function (t) {
 	logger.start();
 	t.end();
 });
+
+test('only call reset if supported by reporter', function (t) {
+	var logger = new Logger();
+	var tapReporter = Tap();
+	tapReporter.reset = undefined;
+	logger.use(tapReporter);
+	logger.reset();
+	t.end();
+});
+
+test('only write if reset is supported by reporter', function (t) {
+	var logger = new Logger();
+	var tapReporter = Tap();
+	tapReporter.reset = undefined;
+	logger.use(tapReporter);
+	logger.write = t.fail;
+	logger.reset();
+	t.end();
+});
+
+test('only call unhandledError if supported by reporter', function (t) {
+	var logger = new Logger();
+	var tapReporter = Tap();
+	tapReporter.unhandledError = undefined;
+	logger.use(tapReporter);
+	logger.unhandledError();
+	t.end();
+});
+
+test('only write if unhandledError is supported by reporter', function (t) {
+	var logger = new Logger();
+	var tapReporter = Tap();
+	tapReporter.unhandledError = undefined;
+	logger.use(tapReporter);
+	logger.write = t.fail;
+	logger.unhandledError();
+	t.end();
+});
+
+test('only call finish if supported by reporter', function (t) {
+	var logger = new Logger();
+	var tapReporter = Tap();
+	tapReporter.finish = undefined;
+	logger.use(tapReporter);
+	logger.finish();
+	t.end();
+});
+
+test('only write if finish is supported by reporter', function (t) {
+	var logger = new Logger();
+	var tapReporter = Tap();
+	tapReporter.finish = undefined;
+	logger.use(tapReporter);
+	logger.write = t.fail;
+	logger.finish();
+	t.end();
+});
+
+test('only call write if supported by reporter', function (t) {
+	var logger = new Logger();
+	var tapReporter = Tap();
+	tapReporter.write = undefined;
+	logger.use(tapReporter);
+	logger.write();
+	t.end();
+});
+
+test('only call stdout if supported by reporter', function (t) {
+	var logger = new Logger();
+	var tapReporter = Tap();
+	tapReporter.stdout = undefined;
+	logger.use(tapReporter);
+	logger.stdout();
+	t.end();
+});
+
+test('only call stderr if supported by reporter', function (t) {
+	var logger = new Logger();
+	var tapReporter = Tap();
+	tapReporter.stderr = undefined;
+	logger.use(tapReporter);
+	logger.stderr();
+	t.end();
+});
