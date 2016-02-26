bc493644cf6615725827c558d830b185d1bd869e
Stephen Sorensen <stephen.sorensen@theice.com>
2016-02-26 09:38:10 -0500

    added tests for customizable babel config

--- test/caching-precompiler.js
+++ test/caching-precompiler.js
@@ -3,6 +3,8 @@ var fs = require('fs');
 var path = require('path');
 var test = require('tap').test;
 var uniqueTempDir = require('unique-temp-dir');
+var sinon = require('sinon');
+var babel = require('babel-core');
 
 var CachingPrecompiler = require('../lib/caching-precompiler');
 
@@ -18,9 +20,14 @@ function endsWithMap(filename) {
 	return /\.js$/.test(filename);
 }
 
+test('before', t => {
+	sinon.spy(babel, 'transform');
+	t.end();
+});
+
 test('creation with new', function (t) {
 	var tempDir = uniqueTempDir();
-	var precompiler = new CachingPrecompiler(tempDir);
+	var precompiler = new CachingPrecompiler(tempDir, null);
 	t.is(precompiler.cacheDir, tempDir);
 	t.end();
 });
@@ -28,14 +35,14 @@ test('creation with new', function (t) {
 test('must be called with new', function (t) {
 	t.throws(function () {
 		var cachingPrecompiler = CachingPrecompiler;
-		cachingPrecompiler(uniqueTempDir());
+		cachingPrecompiler(uniqueTempDir(), null);
 	}, {message: 'Class constructor CachingPrecompiler cannot be invoked without \'new\''});
 	t.end();
 });
 
 test('adds files and source maps to the cache directory as needed', function (t) {
 	var tempDir = uniqueTempDir();
-	var precompiler = new CachingPrecompiler(tempDir);
+	var precompiler = new CachingPrecompiler(tempDir, null);
 
 	t.false(fs.existsSync(tempDir), 'cache directory is not created before it is needed');
 
@@ -46,5 +53,91 @@ test('adds files and source maps to the cache directory as needed', function (t)
 	t.is(files.length, 2);
 	t.is(files.filter(endsWithJs).length, 1, 'one .js file is saved to the cache');
 	t.is(files.filter(endsWithMap).length, 1, 'one .map file is saved to the cache');
+	t.end();
+});
+
+test('uses default babel options when !babelConfig', function (t) {
+	var tempDir = uniqueTempDir();
+	var precompiler = new CachingPrecompiler(tempDir, null);
+	babel.transform.reset();
+
+	precompiler.precompileFile(fixture('es2015.js'));
+
+	t.true(babel.transform.calledOnce);
+	var options = babel.transform.firstCall.args[1];
+
+	t.true('filename' in options);
+	t.true(options.sourceMaps);
+	t.false(options.ast);
+	t.true('inputSourceMap' in options);
+	t.false(options.babelrc);
+	t.true(Array.isArray(options.presets));
+	t.true(Array.isArray(options.plugins));
+	t.end();
+});
+
+test('uses default babel options when babelConfig === "default"', function (t) {
+	var tempDir = uniqueTempDir();
+	var precompiler = new CachingPrecompiler(tempDir, 'default');
+	babel.transform.reset();
+
+	precompiler.precompileFile(fixture('es2015.js'));
+
+	t.true(babel.transform.calledOnce);
+	var options = babel.transform.firstCall.args[1];
+
+	t.true('filename' in options);
+	t.true(options.sourceMaps);
+	t.false(options.ast);
+	t.true('inputSourceMap' in options);
+	t.false(options.babelrc);
+	t.true(Array.isArray(options.presets));
+	t.true(Array.isArray(options.plugins));
+	t.end();
+});
+
+test('allows babel config from package.json/babel when babelConfig === "inherit"', function (t) {
+	var tempDir = uniqueTempDir();
+	var precompiler = new CachingPrecompiler(tempDir, 'inherit');
+	babel.transform.reset();
+
+	precompiler.precompileFile(fixture('es2015.js'));
+
+	t.true(babel.transform.calledOnce);
+	var options = babel.transform.firstCall.args[1];
+
+	t.true('filename' in options);
+	t.true(options.sourceMaps);
+	t.false(options.ast);
+	t.true('inputSourceMap' in options);
+	t.true(options.babelrc);
+	t.end();
+});
+
+test('uses babelConfig for babel options when babelConfig is an object', function (t) {
+	var tempDir = uniqueTempDir();
+	var precompiler = new CachingPrecompiler(tempDir, {
+		presets: ['stage-2', 'es2015'],
+		plugins: []
+	});
+	babel.transform.reset();
+
+	precompiler.precompileFile(fixture('es2015.js'));
+
+	t.true(babel.transform.calledOnce);
+	var options = babel.transform.firstCall.args[1];
+
+	t.true('filename' in options);
+	t.true(options.sourceMaps);
+	t.false(options.ast);
+	t.true('inputSourceMap' in options);
+	t.false(options.babelrc);
+	t.deepEqual(options.presets, ['stage-2', 'es2015']);
+	t.deepEqual(options.plugins, []);
+	t.end();
+});
+
+test('after', t => {
+	babel.transform.restore();
 	t.end();
 });
