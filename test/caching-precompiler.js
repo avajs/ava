421231f9b77ad69c8597bf48f98af658f1682063
James Talmage <james@talmage.io>
2016-06-18 00:38:11 -0400

    extract babel config generation into it's own submodule

--- test/caching-precompiler.js
+++ test/caching-precompiler.js
@@ -5,8 +5,6 @@ var test = require('tap').test;
 var uniqueTempDir = require('unique-temp-dir');
 var sinon = require('sinon');
 var babel = require('babel-core');
-var transformRuntime = require('babel-plugin-transform-runtime');
-var throwsHelper = require('babel-plugin-ava-throws-helper');
 var fromMapFileSource = require('convert-source-map').fromMapFileSource;
 
 var CachingPrecompiler = require('../lib/caching-precompiler');
@@ -119,34 +117,6 @@ test('allows babel config from package.json/babel when babelConfig === "inherit"
 	t.false(options.ast);
 	t.true('inputSourceMap' in options);
 	t.true(options.babelrc);
-	t.end();
-});
-
-test('uses babelConfig for babel options when babelConfig is an object', function (t) {
-	var tempDir = uniqueTempDir();
-	var customPlugin = sinon.stub().returns({visitor: {}});
-	var powerAssert = sinon.stub().returns({visitor: {}});
-	var rewrite = sinon.stub().returns({visitor: {}});
-	var precompiler = new CachingPrecompiler(tempDir, {
-		presets: ['stage-2', 'es2015'],
-		plugins: [customPlugin]
-	});
-	sinon.stub(precompiler, '_createEspowerPlugin').returns(powerAssert);
-	sinon.stub(precompiler, '_createRewritePlugin').returns(rewrite);
-	babel.transform.reset();
-
-	precompiler.precompileFile(fixture('es2015.js'));
-
-	t.true(babel.transform.calledOnce);
-	var options = babel.transform.firstCall.args[1];
-
-	t.true('filename' in options);
-	t.true(options.sourceMaps);
-	t.false(options.ast);
-	t.true('inputSourceMap' in options);
-	t.false(options.babelrc);
-	t.strictDeepEqual(options.presets, ['stage-2', 'es2015']);
-	t.strictDeepEqual(options.plugins, [customPlugin, powerAssert, throwsHelper, rewrite, transformRuntime]);
 	t.end();
 });
 
