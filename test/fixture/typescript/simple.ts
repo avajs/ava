2708e4053d9f32563382a2a96e09437fdfdef4d9
Huan LI <zixia@zixia.net>
2016-11-23 02:13:38 +0000

    follow requested changes #1122 #1109

--- test/fixture/typescript/simple.ts
+++ test/fixture/typescript/simple.ts
@@ -1,6 +1,6 @@
-import { test } from 'ava'
+import { test } from 'ava';
 
 test('AVA run TypeScript without tsc', t => {
-  let i: number = 42
-  t.is(i, <number>42, 'meaning of life')
-})
+  let i: number = 42;
+  t.is(i, <number>42, 'meaning of life');
+});
