beb21c27cde233f823dddfbc1d2f904461b81524
Yuta Hiroto <git@about-hiroppy.com>
2017-07-15 20:36:13 +0900

    Add tests for improper-usage-messages

---
+++ test/reporters/improper-usage-messages.js
@@ -0,0 +1,20 @@
+'use strict';
+const test = require('tap').test;
+const improperUsageMessages = require('../../lib/reporters/improper-usage-messages');
+
+test('results when nothing is applicable', t => {
+	const err = {
+		assertion: 'assertion',
+		improperUsage: {
+			name: 'VersionMismatchError',
+			snapPath: 'path',
+			snapVersion: 2,
+			expectedVersion: 1
+		}
+	};
+
+	const actualOutput = improperUsageMessages.forError(err);
+
+	t.is(actualOutput, null);
+	t.end();
+});
