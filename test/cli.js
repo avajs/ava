b6eef5ac30e7839371a420f17060f62f971717aa
tdeschryver <timdeschryver@outlook.com>
2017-08-12 17:22:05 +0200

    Fail hard when `--concurrency` is set to invalid values (#1478)
    * Update warning message when the flag is used without a value
    * Detect non-integer or negative integer values

--- test/cli.js
+++ test/cli.js
@@ -414,17 +414,47 @@ test('bails when config contains `"tap": true` and `"watch": true`', t => {
 });
 
 ['--concurrency', '-c'].forEach(concurrencyFlag => {
-	test(`bails when ${concurrencyFlag} provided without value`, t => {
+	test(`bails when ${concurrencyFlag} is provided without value`, t => {
 		execCli(['test.js', concurrencyFlag], {dirname: 'fixture/concurrency'}, (err, stdout, stderr) => {
 			t.is(err.code, 1);
-			t.match(stderr, 'The --concurrency and -c flags must be provided the maximum number of test files to run at once.');
+			t.match(stderr, 'The --concurrency and -c flags must be provided.');
 			t.end();
 		});
 	});
 });
 
 ['--concurrency', '-c'].forEach(concurrencyFlag => {
-	test(`works when ${concurrencyFlag} provided with value`, t => {
+	test(`bails when ${concurrencyFlag} is provided with an input that is a string`, t => {
+		execCli([`${concurrencyFlag}=foo`, 'test.js', concurrencyFlag], {dirname: 'fixture/concurrency'}, (err, stdout, stderr) => {
+			t.is(err.code, 1);
+			t.match(stderr, 'The --concurrency and -c flags must be a nonnegative integer.');
+			t.end();
+		});
+	});
+});
+
+['--concurrency', '-c'].forEach(concurrencyFlag => {
+	test(`bails when ${concurrencyFlag} is provided with an input that is a float`, t => {
+		execCli([`${concurrencyFlag}=4.7`, 'test.js', concurrencyFlag], {dirname: 'fixture/concurrency'}, (err, stdout, stderr) => {
+			t.is(err.code, 1);
+			t.match(stderr, 'The --concurrency and -c flags must be a nonnegative integer.');
+			t.end();
+		});
+	});
+});
+
+['--concurrency', '-c'].forEach(concurrencyFlag => {
+	test(`bails when ${concurrencyFlag} is provided with an input that is negative`, t => {
+		execCli([`${concurrencyFlag}=-1`, 'test.js', concurrencyFlag], {dirname: 'fixture/concurrency'}, (err, stdout, stderr) => {
+			t.is(err.code, 1);
+			t.match(stderr, 'The --concurrency and -c flags must be a nonnegative integer.');
+			t.end();
+		});
+	});
+});
+
+['--concurrency', '-c'].forEach(concurrencyFlag => {
+	test(`works when ${concurrencyFlag} is provided with a value`, t => {
 		execCli([`${concurrencyFlag}=1`, 'test.js'], {dirname: 'fixture/concurrency'}, err => {
 			t.ifError(err);
 			t.end();
