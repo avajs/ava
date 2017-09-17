3b3374c17ab40d3893d4c7f410c753fe69558494
Mike Pennisi <mike@mikepennisi.com>
2017-09-17 16:10:45 -0400

    fixup! Stabilize colors

--- test/helper/colors.js
+++ test/helper/colors.js
@@ -8,7 +8,7 @@ const ansiStyles = require('ansi-styles');
 
 function make(name) {
 	const style = ansiStyles[name];
-	return function(string) {
+	return function (string) {
 		return style.open + string + style.close;
 	};
 }
@@ -19,8 +19,8 @@ const gray = make('gray');
 
 module.exports = {
 	blue: make('blue'),
-	boldWhite: (string) => bold(white(string)),
-	dimGray: (string) => gray(dim(string)),
+	boldWhite: string => bold(white(string)),
+	dimGray: string => gray(dim(string)),
 	gray: make('gray'),
 	green: make('green'),
 	magenta: make('magenta'),
