3e68bd60c7951ad1e8198971686f7bd3ba58a5a7
Mike Pennisi <mike@mikepennisi.com>
2017-09-18 14:29:37 -0400

    fixup! Stabilize colors

--- test/helper/colors.js
+++ test/helper/colors.js
@@ -14,14 +14,25 @@ function make(name) {
 }
 const bold = make('bold');
 const white = make('white');
-const dim = make('dim');
 const gray = make('gray');
 
+// The following color definitions are contextual so that they produce expected
+// values which mimic the behavior of the Chalk library.
+const isSimpleWindowsTerm = process.platform === 'win32' && !(process.env.TERM || '').toLowerCase().startsWith('xterm');
+const openDim = isSimpleWindowsTerm ? '' : ansiStyles.dim.open;
+const openBlue = isSimpleWindowsTerm ? '\u001B[94m' : ansiStyles.blue.open;
+// "Use `bold` by default on Windows"
+// https://github.com/chalk/chalk/issues/36
+const blue = string => openBlue + string + ansiStyles.blue.close;
+// "(Windows) chalk.gray.dim not visible"
+// https://github.com/chalk/chalk/issues/58
+const dimGray = string => gray(openDim + string + ansiStyles.dim.close);
+
 module.exports = {
-	blue: make('blue'),
+	blue,
 	boldWhite: string => bold(white(string)),
-	dimGray: string => gray(dim(string)),
-	gray: make('gray'),
+	dimGray,
+	gray,
 	green: make('green'),
 	magenta: make('magenta'),
 	red: make('red'),
