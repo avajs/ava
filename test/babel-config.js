d8167ca74380051de718e88d826c84a67ac86b4f
Mark Wubben <mark@novemberborn.net>
2018-01-28 15:40:53 +0000

    Update @ava/stage-4 preset
    Always load syntax plugins, the preset does not necessarily load them if
    the environment supports the language feature natively. The syntax
    plugins can be removed once Babel can parse the syntax unaided.

--- test/babel-config.js
+++ test/babel-config.js
@@ -9,19 +9,7 @@ const uniqueTempDir = require('unique-temp-dir');
 const babelConfigHelper = require('../lib/babel-config');
 
 const fixture = name => path.join(__dirname, 'fixture', name);
-const setNodeVersion = value => Object.defineProperty(process.versions, 'node', {value});
-const resetNodeVersion = setNodeVersion.bind(null, process.versions.node);
-
-// Execute `run` with a given stubbed node version, then reset to the real
-// version.
-function withNodeVersion(version, run) {
-	setNodeVersion(version);
-	const promise = new Promise(resolve => {
-		resolve(run());
-	});
-	promise.then(resetNodeVersion, resetNodeVersion);
-	return promise;
-}
+const NUM_SYNTAX_PLUGINS = 2;
 
 function withNodeEnv(value, run) {
 	assert(!('NODE_ENV' in process.env));
@@ -51,11 +39,7 @@ test('includes testOptions in Babel compilation', t => {
 		.then(result => {
 			const options = result.getOptions();
 			t.false(options.babelrc);
-			if (options.plugins.length === 1) {
-				t.is(options.plugins[0][0].wrapped, custom);
-			} else {
-				t.is(options.plugins[1][0].wrapped, custom);
-			}
+			t.is(options.plugins[NUM_SYNTAX_PLUGINS][0].wrapped, custom);
 			t.is(options.presets[0][0].wrapped, require('@ava/babel-preset-stage-4'));
 			t.is(options.presets[1][0].wrapped, custom);
 			t.is(options.presets[2][0].wrapped, require('@ava/babel-preset-transform-test-files'));
@@ -116,39 +100,6 @@ test('supports .babelrc.js files', t => {
 			t.is(options.presets[0][0].wrapped, require('@ava/babel-preset-stage-4'));
 			t.is(options.presets[1][0].wrapped, require('@ava/babel-preset-transform-test-files'));
 			t.same(options.presets[1][1], {powerAssert: true});
-		});
-});
-
-test('adds babel-plugin-syntax-object-rest-spread for node versions > 8.3.0', t => {
-	const projectDir = fixture('no-babel-config');
-	const cacheDir = path.join(uniqueTempDir(), 'cache');
-
-	return withNodeVersion('9.0.0', () => babelConfigHelper.build(projectDir, cacheDir, {testOptions: {}}, true))
-		.then(result => {
-			const options = result.getOptions();
-			t.is(options.plugins[0][0].wrapped, require('@babel/plugin-syntax-object-rest-spread').default);
-		});
-});
-
-test('adds babel-plugin-syntax-object-rest-spread for node versions == 8.3.0', t => {
-	const projectDir = fixture('no-babel-config');
-	const cacheDir = path.join(uniqueTempDir(), 'cache');
-
-	return withNodeVersion('8.3.0', () => babelConfigHelper.build(projectDir, cacheDir, {testOptions: {}}, true))
-		.then(result => {
-			const options = result.getOptions();
-			t.is(options.plugins[0][0].wrapped, require('@babel/plugin-syntax-object-rest-spread').default);
-		});
-});
-
-test('does not add babel-plugin-syntax-object-rest-spread for node versions < 8.3.0', t => {
-	const projectDir = fixture('no-babel-config');
-	const cacheDir = path.join(uniqueTempDir(), 'cache');
-
-	return withNodeVersion('8.2.0', () => babelConfigHelper.build(projectDir, cacheDir, {testOptions: {}}, true))
-		.then(result => {
-			const options = result.getOptions();
-			t.true(!options.plugins);
 		});
 });
 
