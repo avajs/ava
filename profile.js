0c2892146425c5fe83864e1dce6207c4737b870f
Mark Wubben <mark@novemberborn.net>
2018-01-31 15:44:47 +0000

    Ensure profile.js skips test file compilation like regular runs
    Read the 'compileEnhancements' option from package.json and guard
    against the Babel config helper not returning any config.

--- profile.js
+++ profile.js
@@ -39,7 +39,8 @@ const conf = pkgConf.sync('ava', {
 	defaults: {
 		babel: {
 			testOptions: {}
-		}
+		},
+		compileEnhancements: true
 	}
 });
 
@@ -78,16 +79,18 @@ const cacheDir = findCacheDir({
 	files: [file]
 }) || uniqueTempDir();
 
-babelConfigHelper.build(process.cwd(), cacheDir, babelConfigHelper.validate(conf.babel), true)
+babelConfigHelper.build(process.cwd(), cacheDir, babelConfigHelper.validate(conf.babel), conf.compileEnhancements === true)
 	.then(result => {
-		const precompiler = new CachingPrecompiler({
-			path: cacheDir,
-			getBabelOptions: result.getOptions,
-			babelCacheKeys: result.cacheKeys
-		});
-
 		const precompiled = {};
-		precompiled[file] = precompiler.precompileFile(file);
+		if (result) {
+			const precompiler = new CachingPrecompiler({
+				path: cacheDir,
+				getBabelOptions: result.getOptions,
+				babelCacheKeys: result.cacheKeys
+			});
+
+			precompiled[file] = precompiler.precompileFile(file);
+		}
 
 		const opts = {
 			file,
