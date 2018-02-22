7e5ff0fdda4bcf96cdbacdc67374ebfea19cc304
Mark Wubben <mark@novemberborn.net>
2018-02-22 17:01:38 +0000

    Remove unused fixtures

--- test/fixture/circular-reference-on-assertion.js
+++
@@ -1,7 +0,0 @@
-import test from '../..';
-
-test('test', t => {
-	const circular = ['a', 'b'];
-	circular.push(circular);
-	t.deepEqual([circular, 'c'], [circular, 'd']);
-});
--- test/fixture/syntax-error.js
+++
@@ -1,5 +0,0 @@
-import test from 'ava';
-
-test.(t => {
-	t.pass();
-});
