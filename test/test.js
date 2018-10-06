40fa08e72fe83ca7f8b5fdcbbd1ebff8760a6185
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2018-10-06 17:53:27 -0400

    Add try-commit to test assertions

--- test/test.js
+++ test/test.js
@@ -797,3 +797,328 @@ test('timeout is refreshed on assert', t => {
 		t.is(result.passed, true);
 	});
 });
+
+test('try-commit are present', t => {
+	return ava(a => {
+		a.pass();
+		t.type(a.try, Function);
+	}).run();
+});
+
+test('try-commit works', t => {
+	const instance = ava(a => {
+		return a
+			.try(b => b.pass())
+			.then(res => {
+				t.true(res.passed);
+				res.commit();
+			});
+	});
+
+	return instance.run()
+		.then(result => {
+			t.true(result.passed);
+			t.is(instance.assertCount, 1);
+		});
+});
+
+test('try-commit can discard produced result', t => {
+	return ava(a => {
+		return a
+			.try(b => b.pass())
+			.then(res => {
+				res.discard();
+			});
+	}).run().then(result => {
+		t.false(result.passed);
+		t.ok(result.error);
+		t.match(result.error.message, /without running any assertions/);
+		t.is(result.error.name, 'Error');
+	});
+});
+
+test('try-commit works with values', t => {
+	const testValue1 = 123;
+	const testValue2 = 123;
+
+	return ava(a => {
+		return a
+			.try((b, val1, val2) => {
+				b.is(val1, val2);
+			}, testValue1, testValue2)
+			.then(res => {
+				t.true(res.passed);
+				res.commit();
+			});
+	}).run().then(result => {
+		t.true(result.passed);
+	});
+});
+
+test('try-commit is properly counted', t => {
+	const instance = ava(a => {
+		return a
+			.try(b => b.pass())
+			.then(res => {
+				t.true(res.passed);
+				t.is(instance.pendingAttemptCount, 1);
+				res.commit();
+				t.is(instance.pendingAttemptCount, 0);
+			});
+	});
+
+	return instance.run().then(result => {
+		t.true(result.passed);
+		t.is(instance.attemptCount, 1);
+	});
+});
+
+test('try-commit is properly counted multiple', t => {
+	const instance = ava(a => {
+		return Promise.all([
+			a.try(b => b.pass()),
+			a.try(b => b.pass()),
+			a.try(b => b.pass())
+		])
+			.then(([res1, res2, res3]) => {
+				t.is(instance.pendingAttemptCount, 3);
+				res1.commit();
+				res2.discard();
+				res3.commit();
+				t.is(instance.pendingAttemptCount, 0);
+			});
+	});
+
+	return instance.run().then(result => {
+		t.true(result.passed);
+		t.is(instance.assertCount, 2);
+	});
+});
+
+test('try-commit goes as many levels', t => {
+	t.plan(5);
+	const instance = ava(a => {
+		t.ok(a.try);
+		return a
+			.try(b => {
+				t.ok(b.try);
+				return b
+					.try(c => {
+						t.ok(c.try);
+						c.pass();
+					})
+					.then(res => {
+						res.commit();
+					});
+			})
+			.then(res => {
+				res.commit();
+			});
+	});
+
+	return instance.run().then(result => {
+		t.true(result.passed);
+		t.is(instance.assertCount, 1);
+	});
+});
+
+test('try-commit fails when not committed', t => {
+	return ava(a => {
+		return a
+			.try(b => b.pass())
+			.then(res => {
+				t.true(res.passed);
+			});
+	}).run().then(result => {
+		t.false(result.passed);
+		t.ok(result.error);
+		t.match(result.error.message, /not all attempts were committed/);
+		t.is(result.error.name, 'Error');
+	});
+});
+
+test('try-commit fails when no assertions inside try', t => {
+	return ava(a => {
+		return a
+			.try(() => {})
+			.then(res => {
+				t.false(res.passed);
+				t.ok(res.error);
+				t.match(res.error.message, /Test finished without running any assertions/);
+				t.is(res.error.name, 'Error');
+				res.commit();
+			});
+	}).run().then(result => {
+		t.false(result.passed);
+	});
+});
+
+test('try-commit fails when no assertions inside multiple try', t => {
+	return ava(a => {
+		return Promise.all([
+			a.try(b => b.pass()).then(res1 => {
+				res1.commit();
+				t.true(res1.passed);
+			}),
+			a.try(() => {}).then(res2 => {
+				t.false(res2.passed);
+				t.ok(res2.error);
+				t.match(res2.error.message, /Test finished without running any assertions/);
+				t.is(res2.error.name, 'Error');
+				res2.commit();
+			})
+		]);
+	}).run().then(result => {
+		t.false(result.passed);
+	});
+});
+
+test('test fails when try-commit committed to failed state', t => {
+	return ava(a => {
+		return a.try(b => b.fail()).then(res => {
+			t.false(res.passed);
+			res.commit();
+		});
+	}).run().then(result => {
+		t.false(result.passed);
+	});
+});
+
+test('try-commit has proper titles, when going in depth and width', t => {
+	t.plan(6);
+	return new Test({
+		fn(a) {
+			t.is(a.title, 'foo');
+
+			return Promise.all([
+				a.try(b => {
+					t.is(b.title, 'foo.A0');
+					return Promise.all([
+						b.try(c => t.is(c.title, 'foo.A0.A0')),
+						b.try(c => t.is(c.title, 'foo.A0.A1'))
+					]);
+				}),
+				a.try(b => t.is(b.title, 'foo.A1')),
+				a.try(b => t.is(b.title, 'foo.A2'))
+			]);
+		},
+		failWithoutAssertions: false,
+		metadata: {type: 'test', callback: false},
+		title: 'foo'
+	}).run();
+});
+
+test('try-commit fails when calling commit twice', t => {
+	return ava(a => {
+		return a.try(b => b.pass()).then(res => {
+			res.commit();
+			res.commit();
+		});
+	}).run().then(result => {
+		t.false(result.passed);
+		t.ok(result.error);
+		t.match(result.error.message, /was already called/);
+		t.is(result.error.name, 'Error');
+	});
+});
+
+test('try-commit allows planning inside the try', t => {
+	return ava(a => {
+		return a.try(b => {
+			b.plan(3);
+
+			b.pass();
+			b.pass();
+			b.pass();
+		}).then(res => {
+			t.true(res.passed);
+			res.commit();
+		});
+	}).run().then(result => {
+		t.true(result.passed);
+	});
+});
+
+test('try-commit fails when plan is not reached inside the try', t => {
+	return ava(a => {
+		return a.try(b => {
+			b.plan(3);
+
+			b.pass();
+			b.pass();
+		}).then(res => {
+			t.false(res.passed);
+			res.commit();
+		});
+	}).run().then(result => {
+		t.false(result.passed);
+	});
+});
+
+test('try-commit passes with failing test', t => {
+	return ava.failing(a => {
+		return a
+			.try(b => b.fail())
+			.then(res => {
+				t.false(res.passed);
+				t.ok(res.error);
+				t.match(res.error.message, /Test failed via `t\.fail\(\)`/);
+				t.is(res.error.name, 'AssertionError');
+				res.commit();
+			});
+	}).run().then(result => {
+		t.true(result.passed);
+	});
+});
+
+test('try-commit works with callback test', t => {
+	return ava.cb(a => {
+		a
+			.try(b => b.pass())
+			.then(res => {
+				res.commit();
+				a.end();
+			});
+	}).run().then(result => {
+		t.true(result.passed);
+	});
+});
+
+test('try-commit works with failing callback test', t => {
+	return ava.cb.failing(a => {
+		a
+			.try(b => b.fail())
+			.then(res => {
+				t.false(res.passed);
+				t.ok(res.error);
+				t.match(res.error.message, /Test failed via `t\.fail\(\)`/);
+				t.is(res.error.name, 'AssertionError');
+				res.commit();
+			})
+			.then(() => {
+				a.end();
+			});
+	}).run().then(result => {
+		t.true(result.passed);
+	});
+});
+
+test('try-commit can be discarded', t => {
+	const instance = ava(a => {
+		const p = a.try(b => {
+			return new Promise(resolve => setTimeout(resolve, 500))
+				.then(() => b.pass());
+		});
+
+		p.discard();
+
+		return p.then(res => {
+			t.is(res, null);
+		});
+	});
+
+	return instance.run().then(result => {
+		t.false(result.passed);
+		t.is(instance.assertCount, 0);
+	});
+});
