avajs/ava

commit 223f992867b2ed8fb7e14c36da8e867efd7b74f4

Fix snapshot report merging
Rename `newLength` to `checkPosition` to make the function clearer.
At the end of the loop `checkPosition` points to the last index that
does not contain a line ending, so add one to include it.

Fixes #1848

lib/snapshot-manager.js
@@ -82,11 +82,11 @@ function tryRead(file) {
 }
 
 function withoutLineEndings(buffer) {
-	let newLength = buffer.byteLength - 1;
-	while (buffer[newLength] === 0x0A || buffer[newLength] === 0x0D) {
-		newLength--;
+	let checkPosition = buffer.byteLength - 1;
+	while (buffer[checkPosition] === 0x0A || buffer[checkPosition] === 0x0D) {
+		checkPosition--;
 	}
-	return buffer.slice(0, newLength);
+	return buffer.slice(0, checkPosition + 1);
 }
 
 function formatEntry(label, descriptor) {

commit 2f9ecf8843a89a170c957f97e706ed7324060c0b

Insert a trailing newline when appending new snapshot entries
I reckon this fixes a regression introduced in https://github.com/avajs/ava/pull/1852.

lib/snapshot-manager.js
@@ -151,6 +151,8 @@ function appendReportEntries(existingReport, entries) {
 	buffers.unshift(prepend);
 	byteLength += prepend.byteLength;
 
+	buffers.push(REPORT_TRAILING_NEWLINE);
+	byteLength += REPORT_TRAILING_NEWLINE.byteLength;
 	return Buffer.concat(buffers, byteLength);
 }
 