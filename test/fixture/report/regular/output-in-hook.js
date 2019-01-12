e94afa7e767c42f9e872fe0f67c26762f6af05fb
Daniel Lupu <lupu.daniel.f@gmail.com>
2019-01-12 20:48:10 +0200

    fix t.log in hooks

---
+++ test/fixture/report/regular/output-in-hook.js
@@ -0,0 +1,35 @@
+import test from '../../../..';
+
+test.before(() => {});
+
+test.before(t => {
+	t.log('before');
+});
+
+test.after('cleanup', t => {
+	t.log('after');
+});
+
+test.after.always('cleanup', t => {
+	t.log('afterAlways');
+});
+
+test.beforeEach(t => {
+	t.log('beforeEach');
+});
+
+test.afterEach(t => {
+	t.log('afterEach');
+});
+
+test.afterEach.always(t => {
+	t.log('afterEachAlways');
+});
+
+test('passing test', t => {
+	t.pass();
+});
+
+test('failing test', t => {
+	t.fail();
+});
