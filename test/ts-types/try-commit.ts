cc5d4ae17f136e4a7f3ef0eb3736e97c21736156
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2019-03-17 20:16:30 -0400

    Test types of try fn

---
+++ test/ts-types/try-commit.ts
@@ -0,0 +1,76 @@
+import test, {ExecutionContext, Macro} from "../..";
+
+{
+	test("attempt", async t => {
+		const attempt = await t.try(
+			(u, a, b) => {
+				u.is(a.length, b);
+			},
+			"string",
+			6
+		);
+		attempt.commit();
+	});
+
+	test("attempt with title", async t => {
+		const attempt = await t.try(
+			"attempt title",
+			(u, a, b) => {
+				u.is(a.length, b);
+			},
+			"string",
+			6
+		);
+		attempt.commit();
+	});
+}
+
+{
+	const lengthCheck = (t: ExecutionContext, a: string, b: number) => {
+		t.is(a.length, b);
+	};
+
+	test("attempt with helper", async t => {
+		const attempt = await t.try(lengthCheck, "string", 6);
+		attempt.commit();
+	});
+
+	test("attempt with title", async t => {
+		const attempt = await t.try(lengthCheck, "string", 6);
+		attempt.commit();
+	});
+}
+
+{
+	test("all possible variants to pass to t.try", async t => {
+		// no params
+		t.try(tt => tt.pass());
+		/* fails as expected */ // t.try([]);
+		t.try([tt => tt.pass()]);
+		t.try([tt => tt.pass(), tt => tt.fail()]);
+
+		t.try("test", tt => tt.pass());
+		/* fails as expected */ // t.try("test", []);
+		t.try("test", [tt => tt.pass()]);
+		t.try("test", [tt => tt.pass(), tt => tt.fail()]);
+
+		// some params
+		t.try((tt, a, b) => tt.is(a.length, b), "hello", 5);
+		/* fails as expected */ // t.try([], "hello", 5);
+		t.try([(tt, a, b) => tt.is(a.length, b)], "hello", 5);
+		t.try([(tt, a, b) => tt.is(a.length, b), (tt, a, b) => tt.is(a.slice(b), "")], "hello", 5);
+
+		t.try("test", (tt, a, b) => tt.is(a.length, b), "hello", 5);
+		/* fails as expected */ // t.try("test", [], "hello", 5);
+		t.try("test", [(tt, a, b) => tt.is(a.length, b)], "hello", 5);
+		t.try("test", [(tt, a, b) => tt.is(a.length, b), (tt, a, b) => tt.is(a.slice(b), "")], "hello", 5);
+
+		// macro with title
+		const macro1: Macro<[string, number]> = (tt, a, b) => tt.is(a.length, b);
+		macro1.title = (title, a, b) => `${title ? `${title} `: ''}str: "${a}" with len: "${b}"`;
+		const macro2: Macro<[string, number]> = (tt, a, b) => tt.is(a.slice(b), '');
+
+		t.try([macro1, macro2], 'hello', 5);
+		t.try('title', [macro1, macro2], 'hello', 5);
+	});
+}
