6639ba82e68d10a86ca00dce656caf3ae30ef1f5
Alexis Fontaine <alexis.fontaine@outlook.com>
2019-05-27 11:55:03 +0200

    Check that environment variables are all string values

--- test/integration/environment-variables.js
+++ test/integration/environment-variables.js
@@ -1,5 +1,6 @@
 'use strict';
 const {test} = require('tap');
+const figures = require('figures');
 const {execCli} = require('../helper/cli');
 const {name, value} = require('../fixture/environment-variables');
 
@@ -17,6 +18,19 @@ test('overrides environment variables provided through the CLI', t => {
 	execCli(['test.js'], {dirname: 'fixture/environment-variables', env}, (err, stdout) => {
 		t.ifError(err);
 		t.match(stdout, /1 test passed/);
+		t.end();
+	});
+});
+
+test('errors if environment variables are not string values', t => {
+	execCli(['es2015.js'], {dirname: 'fixture/invalid-environment-variables'}, (err, stdout, stderr) => {
+		t.ok(err);
+
+		let expectedOutput = '\n';
+		expectedOutput += figures.cross + ' The \'environmentVariables\' configuration must be an object containing string values.';
+		expectedOutput += '\n';
+
+		t.is(stderr, expectedOutput);
 		t.end();
 	});
 });
