c8c7441e17b43971a26b6e0363967af09f5bb140
Mark Wubben <mark@novemberborn.net>
2019-06-10 16:03:39 +0200

    Upgrade tap
    Use tap's nyc integration.

--- test/promise.js
+++ test/promise.js
@@ -1,6 +1,6 @@
 'use strict';
-require('../lib/chalk').set();
-require('../lib/worker/options').set({color: false});
+require('../lib/chalk').set({enabled: false});
+require('../lib/worker/options').set({});
 
 const Promise = require('bluebird');
 const {test} = require('tap');
