8e51406097b20a80842b2b976770ab9a0ae00145
Maksim Elkin <melkin@powerofgeeks.com>
2019-11-25 02:05:21 +0300

    node-arguments simplify processing

--- test/node-arguments.js
+++ test/node-arguments.js
@@ -4,28 +4,24 @@ const {test} = require('tap');
 const normalizeNodeArguments = require('../lib/node-arguments');
 
 test('normalizes multiple node arguments from cli', t => {
-	t.deepEqual(normalizeNodeArguments('"--a --b --c"'), ['--a', '--b', '--c']);
+	t.deepEqual(normalizeNodeArguments([], '--a --b --c'), {a: true, b: true, c: true});
 	t.end();
 });
 
 test('normalizes multiple node arguments from config', t => {
-	t.deepEqual(normalizeNodeArguments(['--arg1', '--b']), ['--arg1', '--b']);
+	t.deepEqual(normalizeNodeArguments(['--arg1', '--b'], ''), {arg1: true, b: true});
+	t.end();
+});
+
+test('normalizes node arguments from config and cli', t => {
+	t.deepEqual(
+		normalizeNodeArguments(['--arg1', '--b=2'], '--arg2 --b=12'),
+		{arg1: true, arg2: true, b: 12}
+	);
 	t.end();
 });
 
 test('normalizes single node arguments from cli', t => {
-	t.deepEqual(normalizeNodeArguments('--test-flag'), ['--test-flag']);
+	t.deepEqual(normalizeNodeArguments([], '--test-flag'), {'test-flag': true});
 	t.end();
 });
-
-test('removes extra inspect', t => {
-	process.execArgv = ['--inspect-brk=123'];
-	t.deepEqual(normalizeNodeArguments('--inspect'), ['--inspect']);
-	t.end();
-});
-
-test('fails on inspect with port', t => {
-	t.throws(() => normalizeNodeArguments('--inspect=9230'), 'The \'nodeArguments\' configuration must not contain inspect with port.');
-	t.end();
-});
-
