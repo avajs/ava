b31ff7fee3088437aec8f51dd089bcb0300b4e34
Mark Wubben <mark@novemberborn.net>
2020-01-04 18:26:24 +0100

    Remove the faking of the TTY in worker processes

--- test/fixture/tty/get-color-depth-missing.js
+++
@@ -1,9 +0,0 @@
-const test = require('../../..');
-
-const assertNoGetColorDepth = (t, stream) => {
-	t.true(stream.isTTY);
-	t.is(stream.getColorDepth, undefined);
-};
-
-test('stderr does not implement getColorDepth', assertNoGetColorDepth, process.stderr);
-test('stdout does not implement getColorDepth', assertNoGetColorDepth, process.stdout);
--- test/helper/simulate-tty.js
+++ test/helper/simulate-tty.js
@@ -1,5 +1,19 @@
-'use strict';
-const makeHasColors = require('../../lib/worker/fake-tty-has-colors');
+const tty = require('tty');
+
+// Call original method to ensure the correct errors are thrown.
+const assertHasColorsArguments = count => {
+	tty.WriteStream.prototype.hasColors(count);
+};
+
+const makeHasColors = colorDepth => (count = 16, env = undefined) => {
+	// `count` is optional too, so make sure it's not an env object.
+	if (env === undefined && typeof count === 'object' && count !== null) {
+		count = 16;
+	}
+
+	assertHasColorsArguments(count);
+	return count <= 2 ** colorDepth;
+};
 
 const simulateTTY = (stream, colorDepth, hasColors) => {
 	stream.isTTY = true;
