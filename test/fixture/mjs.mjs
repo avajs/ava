bc7a7d6c1af2e73f09881bce2ffcd1f36554c490
Markus Felten <Markus.Felten@gmx.de>
2020-01-27 10:02:52 +0100

    style: add hints to make import '.../index.js' pass

--- test/fixture/mjs.mjs
+++ test/fixture/mjs.mjs
@@ -1,4 +1,4 @@
-import test from '../../index.js';
+import test from '../../index.js'; // eslint-disable-line import/no-useless-path-segments, unicorn/import-index, import/extensions
 
 test('pass', t => {
 	t.pass();
