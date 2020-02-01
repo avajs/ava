531eb1e9bfa19af0353fcab2b5d0e6cf7cb73609
Markus Felten <Markus.Felten@db.com>
2020-02-01 21:48:00 +0100

    style: make xo happy

--- test/integration/assorted.js
+++ test/integration/assorted.js
@@ -166,7 +166,7 @@ test('selects .cjs test files', t => {
 
 test('load .mjs test files (when node supports it)', t => {
 	execCli('mjs.mjs', (err, stdout) => {
-		if (Number.parseFloat(process.version.slice(1)) >= 13 /*&& process.platform !== 'win32'*/) {
+		if (Number.parseFloat(process.version.slice(1)) >= 13) {
 			t.ifError(err);
 			t.match(stdout, /1 test passed/);
 			t.end();
@@ -180,7 +180,7 @@ test('load .mjs test files (when node supports it)', t => {
 
 test('load .js test files as ESM modules (when node supports it)', t => {
 	execCli('test.js', {dirname: 'fixture/esm'}, (err, stdout) => {
-		if (Number.parseFloat(process.version.slice(1)) >= 13 /*&& process.platform !== 'win32'*/) {
+		if (Number.parseFloat(process.version.slice(1)) >= 13) {
 			t.ifError(err);
 			t.match(stdout, /1 test passed/);
 			t.end();
