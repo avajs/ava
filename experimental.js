d74c83f0cb627b1ea69c67d87890f1a92381046e
Mark Wubben <mark@novemberborn.net>
2020-03-29 12:46:48 +0200

    New experimental test interfaces experiment!
    With this commit, test.cb() has been removed. See https://github.com/avajs/ava/issues/2387

--- experimental.js
+++ experimental.js
@@ -1,8 +1,9 @@
 'use strict';
+const path = require('path');
 
 // Ensure the same AVA install is loaded by the test file as by the test worker
 if (process.env.AVA_PATH && process.env.AVA_PATH !== __dirname) {
-	module.exports = require(process.env.AVA_PATH);
+	module.exports = require(path.join(process.env.AVA_PATH, 'experimental.js'));
 } else {
-	module.exports = require('./lib/worker/main').ava3();
+	module.exports = require('./lib/worker/main').experimental();
 }
