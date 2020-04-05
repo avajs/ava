a842fd29f7d8b2253dd1fec9b74cc4a6e72d8974
bruno pereira <bruno.webjr@gmail.com>
2020-04-05 14:35:11 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Ensure the same AVA install is loaded by the test file as by the test worker
-process.env.AVA_PATH !== __dirname ?
-	module.exports = require(process.env.AVA_PATH) :
+if (process.env.AVA_PATH !== __dirname) {
+	module.exports = require(process.env.AVA_PATH)
+} else {
 	module.exports = require('./lib/worker/main')
+}
