a536b563688b07484152b67811c62563256446f8
Mark Wubben <mark@novemberborn.net>
2020-04-11 13:10:14 +0200

    Remove API tests, use integration tests

--- test-tap/fixture/one-pass-one-fail.js
+++
@@ -1,9 +0,0 @@
-const test = require('../..');
-
-test('this is a passing test', t => {
-	t.pass();
-});
-
-test('this is a failing test', t => {
-	t.fail();
-});
--- test-tap/integration/line-numbers.js
+++ test-tap/integration/line-numbers.js
@@ -2,15 +2,61 @@
 const {test} = require('tap');
 const {execCli} = require('../helper/cli');
 
-test('can select tests by line numbers', t => {
+test('select test by line number', t => {
 	execCli([
 		'line-numbers.js',
-		'line-numbers.js:8',
-		'line-numbers.js:7-15',
-		'one-pass-one-fail.js:5'
+		'line-numbers.js:3'
 	], (error, stdout) => {
 		t.ifError(error);
-		t.match(stdout, /3 tests passed\s+1 test todo/);
+		t.match(stdout, /unicorn/);
+		t.match(stdout, /1 test passed/);
+		t.notMatch(stdout, /todo/);
+		t.end();
+	});
+});
+
+test('select serial test by line number', t => {
+	execCli([
+		'line-numbers.js:12'
+	], (error, stdout) => {
+		t.ifError(error);
+		t.match(stdout, /cat/);
+		t.match(stdout, /1 test passed/);
+		t.notMatch(stdout, /todo/);
+		t.end();
+	});
+});
+
+test('select todo test by line number', t => {
+	execCli([
+		'line-numbers.js:15'
+	], (error, stdout) => {
+		t.ifError(error);
+		t.match(stdout, /dog/);
+		t.match(stdout, /1 test todo/);
+		t.end();
+	});
+});
+
+test('select tests by line number range', t => {
+	execCli([
+		'line-numbers.js:5-7'
+	], (error, stdout) => {
+		t.ifError(error);
+		t.match(stdout, /unicorn/);
+		t.match(stdout, /rainbow/);
+		t.match(stdout, /2 tests passed/);
+		t.notMatch(stdout, /todo/);
+		t.end();
+	});
+});
+
+test('no test selected by line number', t => {
+	execCli([
+		'line-numbers.js:6'
+	], (error, stdout) => {
+		t.ok(error);
+		t.match(stdout, /No tests selected by line numbers/);
 		t.end();
 	});
 });
