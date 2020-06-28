8b0872a760f57bd5eb507355185c5fd3594ef128
Mark Wubben <mark@novemberborn.net>
2020-06-28 15:39:40 +0200

    Prefer snapshots for messages, but strip platform-specific figures

--- test/snapshot-updates/test.js
+++ test/snapshot-updates/test.js
@@ -1,7 +1,5 @@
 const test = require('@ava/test');
 const exec = require('../helpers/exec');
-const figures = require('figures');
-const chalk = require('chalk');
 
 test('cannot update snapshots when file contains skipped tests', async t => {
 	const result = await t.throwsAsync(exec.fixture('contains-skip.js', '-u'));
@@ -17,11 +15,14 @@ test('cannot update snapshots when file contains exclusive tests', async t => {
 	t.snapshot(result.stats.unsavedSnapshots, 'files where snapshots could not be updated');
 });
 
-async function testInvalidInput(t, promise, expectedMessage) {
-	const result = await t.throwsAsync(promise);
-	t.is(`${chalk.red(figures.cross)} ${expectedMessage}`, result.stderr.trim());
-}
+const stripLeadingFigures = string => string.replace(/^\W+/, '');
 
-test('cannot update snapshots when matching test titles', testInvalidInput, exec.fixture('contains-skip.js', '-u', '-m=snapshot'), 'Snapshots cannot be updated when matching specific tests.');
+test('cannot update snapshots when matching test titles', async t => {
+	const result = await t.throwsAsync(exec.fixture('contains-skip.js', '-u', '-m=snapshot'));
+	t.snapshot(stripLeadingFigures(result.stderr.trim()));
+});
 
-test('cannot update snapshots when selecting tests by line number', testInvalidInput, exec.fixture('contains-skip.js:4', '-u'), 'Snapshots cannot be updated when selecting specific tests by their line number.');
+test('cannot update snapshots when selecting tests by line number', async t => {
+	const result = await t.throwsAsync(exec.fixture('contains-skip.js:4', '-u'));
+	t.snapshot(stripLeadingFigures(result.stderr.trim()));
+});
