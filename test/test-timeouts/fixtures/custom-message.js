a557bc08c49b11fe0faa40ea0095d21c187757ec
Mark Wubben <mark@novemberborn.net>
2020-07-07 17:33:27 +0200

    Reinstall XO to fix CI error

--- test/test-timeouts/fixtures/custom-message.js
+++ test/test-timeouts/fixtures/custom-message.js
@@ -1,6 +1,6 @@
 const test = require('ava');
 
 test('timeout with custom message', async t => {
-	t.timeout(10, 'time budget exceeded'); // eslint-disable-line ava/assertion-arguments
+	t.timeout(10, 'time budget exceeded');
 	await new Promise(() => {});
 });
