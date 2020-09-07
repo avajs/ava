c15f6a204ea4941bcc90364c5c09f11f0fd5ee46
Pedro Augusto de Paula Barbosa <papb1996@gmail.com>
2020-09-07 14:49:49 -0300

    Improve tests

--- test-d/is.ts
+++
@@ -1,9 +0,0 @@
-import {expectError} from 'tsd';
-import {IsAssertion} from '..';
-
-declare const is: IsAssertion;
-
-declare function getLiteralStringUnion(): 'hello' | 'world';
-
-// See #2575 and https://github.com/microsoft/TypeScript/issues/40377
-expectError(is(getLiteralStringUnion(), 'another-string'));
---
+++ test-d/regression-2575.ts
@@ -0,0 +1,16 @@
+import {expectError} from 'tsd';
+import {IsAssertion, NotAssertion, DeepEqualAssertion, NotDeepEqualAssertion} from '..';
+
+declare const is: IsAssertion;
+declare const not: NotAssertion;
+declare const deepEqual: DeepEqualAssertion;
+declare const notDeepEqual: NotDeepEqualAssertion;
+
+declare const literalStringUnionValue: 'hello' | 'world';
+declare const objectWithLiteralStringUnionValue: { foo: 'hello' | 'world' };
+
+// See #2575 and https://github.com/microsoft/TypeScript/issues/40377
+expectError(is(literalStringUnionValue, 'another-string'));
+expectError(not(literalStringUnionValue, 'another-string'));
+expectError(deepEqual(objectWithLiteralStringUnionValue, { foo: 'another-string' }));
+expectError(notDeepEqual(objectWithLiteralStringUnionValue, { foo: 'another-string' }));
