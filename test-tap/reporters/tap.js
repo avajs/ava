f42e0e03a4f6f34d81a2f158b18af9f0a90b71bf
Mark Wubben <mark@novemberborn.net>
2020-09-08 10:39:57 +0200

    Sanitize timers.js positions in reporter logs

--- test-tap/reporters/tap.js
+++ test-tap/reporters/tap.js
@@ -14,7 +14,7 @@ const run = (type, sanitizers = []) => t => {
 
 	const tty = new TTYStream({
 		columns: 200,
-		sanitizers: [...sanitizers, report.sanitizers.cwd, report.sanitizers.experimentalWarning, report.sanitizers.posix]
+		sanitizers: [...sanitizers, report.sanitizers.cwd, report.sanitizers.experimentalWarning, report.sanitizers.posix, report.sanitizers.timers]
 	});
 	const reporter = new TapReporter({
 		projectDir: report.projectDir(type),
