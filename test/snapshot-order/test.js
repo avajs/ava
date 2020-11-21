bec3485f5502b4bc36894e79a28b5565f47c2eec
ninevra <ninevra@gmail.com>
2020-11-20 17:34:49 -0800

    Move intertest-order fixture to subdirectory

--- test/snapshot-order/test.js
+++ test/snapshot-order/test.js
@@ -4,18 +4,22 @@ const fs = require('fs');
 const path = require('path');
 
 test('snapshot files are independent of test resolution order', async t => {
+	const options = {
+		cwd: exec.cwd('intertest-order')
+	};
 	// Run, updating snapshots.
-	await exec.fixture(['intertest-order.js', '-u']);
+	await exec.fixture(['test.js', '-u'], options);
 
 	// Read the resulting file
-	const snapshotPath = path.join(__dirname, 'fixtures', 'intertest-order.js.snap');
+	const snapshotPath = path.join(__dirname, 'fixtures', 'intertest-order', 'test.js.snap');
 	const snapshot = fs.readFileSync(snapshotPath);
 
 	// Run in reversed order, updating snapshots.
-	await exec.fixture(['intertest-order.js', '-u'], {
+	await exec.fixture(['test.js', '-u'], {
 		env: {
 			INTERTEST_ORDER_REVERSE: 'true'
-		}
+		},
+		...options
 	});
 
 	// Read the resulting file
