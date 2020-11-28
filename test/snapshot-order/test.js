5afacb4ab787357ac3fbc239383a326b8f4bf7ad
ninevra <ninevra@gmail.com>
2020-11-28 13:02:25 -0800

    Add a larger test case

--- test/snapshot-order/test.js
+++ test/snapshot-order/test.js
@@ -78,6 +78,32 @@ test('snapshot reports are sorted in declaration order', async t => {
 	t.deepEqual(ids, [...ids].sort((a, b) => a - b));
 });
 
+test('deterministic and sorted over a large, random test case', async t => {
+	const options = {
+		cwd: exec.cwd('randomness'),
+		env: {
+			AVA_FORCE_CI: 'not-ci'
+		}
+	};
+
+	const snapshotPath = path.join(options.cwd, 'test.js.snap');
+	const reportPath = path.join(options.cwd, 'test.js.md');
+
+	// Run test
+	await exec.fixture(['--update-snapshots'], options);
+
+	// Assert snapshot is unchanged
+	const snapshot = fs.readFileSync(snapshotPath);
+
+	t.snapshot(snapshot);
+
+	// Assert report is sorted
+	const report = fs.readFileSync(reportPath);
+	const ids = getSnapshotIds(report);
+
+	t.deepEqual(ids, [...ids].sort((a, b) => a - b));
+});
+
 const unsortedSnapshotPath = path.join(__dirname, 'fixtures', 'backwards-compatibility', 'unsorted', 'test.js.snap');
 const unsortedReportPath = path.join(__dirname, 'fixtures', 'backwards-compatibility', 'unsorted', 'test.js.md');
 
