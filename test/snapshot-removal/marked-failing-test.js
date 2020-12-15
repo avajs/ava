39d2860c02ff5b5e757960dfc93daa883e4e312c
ninevra <ninevra@gmail.com>
2020-12-15 00:05:52 -0800

    Remove tests of undesired behavior

--- test/snapshot-removal/marked-failing-test.js
+++
@@ -1,9 +0,0 @@
-const test = require('@ava/test');
-const exec = require('../helpers/exec');
-const {testSnapshotPruning} = require('./helpers/macros');
-
-test.serial('snapshots remain if any test.failing()', testSnapshotPruning, {
-	cwd: exec.cwd('marked-failing-test'),
-	cli: ['--update-snapshots'],
-	remove: false
-});
--- test/snapshot-removal/skipped-hooks.js
+++
@@ -1,9 +0,0 @@
-const test = require('@ava/test');
-const exec = require('../helpers/exec');
-const {testSnapshotPruning} = require('./helpers/macros');
-
-test.serial('snapshots remain if hooks are skipped', testSnapshotPruning, {
-	cwd: exec.cwd('skipped-hooks'),
-	cli: ['--update-snapshots'],
-	remove: false
-});
