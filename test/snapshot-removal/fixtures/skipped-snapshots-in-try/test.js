23a43bbfb9d7bb27e1bead23b166d946ba10fe8d
Mark Wubben <mark@novemberborn.net>
2020-12-31 15:22:24 +0100

    Clarify AVA import in fixtures

--- test/snapshot-removal/fixtures/skipped-snapshots-in-try/test.js
+++ test/snapshot-removal/fixtures/skipped-snapshots-in-try/test.js
@@ -1,4 +1,4 @@
-const test = require(process.env.AVA_PATH || 'ava');
+const test = require(process.env.AVA_PATH); // This fixture is copied to a temporary directory, so require AVA through its configured path.
 
 if (process.env.TEMPLATE) {
 	test('skipped snapshots in try', async t => {
