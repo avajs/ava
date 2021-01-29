62a0e80c1f11e24604711cce58f3b7092433c6b7
Bunyanuch Saengnet <bunyanuch.saengnet@kabelsurfer.com>
2021-01-29 18:46:28 +0000

    Code linting

--- test/scheduler/test.js
+++ test/scheduler/test.js
@@ -4,11 +4,11 @@ const exec = require('../helpers/exec');
 test('failing tests come first', async t => {
 	try {
 		await exec.fixture(['1pass.js', '2fail.js']);
-	}catch (_) { }
+	} catch { }
 
 	try {
 		await exec.fixture(['--concurrency=1', '1pass.js', '2fail.js']);
-	} catch(error) {
+	} catch (error) {
 		t.snapshot(error.stdout);
 	}
 });
