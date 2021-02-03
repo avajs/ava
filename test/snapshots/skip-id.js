cdd29bb7b973d796c0a92df5b4587aa4e545d17d
ninevra <ninevra@gmail.com>
2021-02-03 01:36:16 -0800

    Snapshot the fixture's report file

--- test/snapshots/skip-id.js
+++ test/snapshots/skip-id.js
@@ -1,9 +1,16 @@
 const test = require('@ava/test');
 const exec = require('../helpers/exec');
+const fs = require('fs').promises;
+const path = require('path');
 
 // Regression test for #2662
 test('skipping snapshots with ids works', async t => {
 	const result = await exec.fixture([]);
 	t.snapshot(result.stats.passed, 'passed tests');
-	// TODO snapshot the report file, it's also affected
+
+	const report = await fs.readFile(
+		path.join(__dirname, 'fixtures', 'test.js.md'),
+		{encoding: 'utf8'}
+	);
+	t.snapshot(report, 'snapshot report');
 });
