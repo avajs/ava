d5fb0721c96704f7e5d6cde62a94b607398f8a5d
Bunyanuch Saengnet <bunyanuch.saengnet@kabelsurfer.com>
2021-02-03 19:22:41 +0000

    Remove scheduling in tests

--- test-tap/helper/report.js
+++ test-tap/helper/report.js
@@ -8,6 +8,8 @@ const replaceString = require('replace-string');
 const pkg = require('../../package.json');
 const {normalizeGlobs} = require('../../lib/globs');
 const providerManager = require('../../lib/provider-manager');
+const isCi = require('../../lib/is-ci');
+const scheduler = require('../../lib/scheduler.js');
 
 let _Api = null;
 const createApi = options => {
@@ -31,6 +33,15 @@ const createApi = options => {
 	}
 
 	return new _Api(options);
+};
+
+const prepareScheduler = projectDir => {
+	if (isCi) {
+		return;
+	}
+
+	const path = scheduler.prepareTestResultsFile(projectDir);
+	scheduler.deleteFile(path);
 };
 
 exports.assert = (t, logFile, buffer) => {
@@ -102,6 +113,7 @@ const run = (type, reporter, {match = [], filter} = {}) => {
 
 	options.globs = normalizeGlobs({extensions: options.extensions, files: ['*'], providers: []});
 
+	prepareScheduler(projectDir);
 	const api = createApi(options);
 	api.on('run', plan => reporter.startRun(plan));
 
