cd7ee3eb99b2c0b6a150c3d17f5e39ae11036655
ninevra <ninevra@gmail.com>
2021-02-11 13:18:55 -0800

    Remove type test of removed id override

--- test-d/snapshot.ts
+++ test-d/snapshot.ts
@@ -5,14 +5,10 @@ test('snapshot', t => {
 	t.snapshot({foo: 'bar'});
 	t.snapshot(null, 'a snapshot with a message');
 	expectError(t.snapshot('hello world', null));
-	// See https://github.com/avajs/ava/issues/2669
-	expectError(t.snapshot({foo: 'bar'}, {id: 'an id'}));
 });
 
 test('snapshot.skip', t => {
 	t.snapshot.skip({foo: 'bar'});
 	t.snapshot.skip(null, 'a snapshot with a message');
 	expectError(t.snapshot.skip('hello world', null));
-	// See https://github.com/avajs/ava/issues/2669
-	expectError(t.snapshot.skip({foo: 'bar'}, {id: 'an id'}));
 });
