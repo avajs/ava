69e2f62c1a9bd272b0b98ed7eb149305113183f6
ninevra <ninevra@gmail.com>
2021-02-13 12:57:38 -0800

    Update tests to allow test.skip() with -u

--- test/snapshot-updates/test.js
+++ test/snapshot-updates/test.js
@@ -1,13 +1,6 @@
 const test = require('@ava/test');
 const exec = require('../helpers/exec');
 
-test('cannot update snapshots when file contains skipped tests', async t => {
-	const result = await t.throwsAsync(exec.fixture(['contains-skip.js', '-u']));
-	t.snapshot(result.stats.failed, 'failed tests');
-	t.snapshot(result.stats.skipped, 'skipped tests');
-	t.snapshot(result.stats.unsavedSnapshots, 'files where snapshots could not be updated');
-});
-
 test('cannot update snapshots when file contains exclusive tests', async t => {
 	const result = await exec.fixture(['contains-only.js', '-u']);
 	t.snapshot(result.stats.failed, 'failed tests');
