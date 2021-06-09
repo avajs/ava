544e8e8fd0d0253175c1c3ac4f283bcb122e1fd4
ninevra <ninevra@gmail.com>
2021-06-09 11:29:08 -0700

    Check error message more carefully

--- test/snapshot-tests/corrupt.js
+++ test/snapshot-tests/corrupt.js
@@ -6,12 +6,40 @@ import test from '@ava/test';
 import {cwd, fixture, cleanOutput} from '../helpers/exec.js';
 import {withTemporaryFixture} from '../helpers/with-temporary-fixture.js';
 
+function countMatches(string, regex) {
+	return [...string.matchAll(regex)].length;
+}
+
+function countStringMatches(string, patternString) {
+	if (patternString.length < 1) {
+		throw new RangeError('Pattern must be non-empty');
+	}
+	let index = string.indexOf(patternString);
+	let matches = 0;
+	while (index !== -1 && index < string.length) {
+		matches++;
+		index = string.indexOf(patternString, index + patternString.length);
+	}
+	return matches;
+}
+
 test('snapshot corruption is reported to the console', async t => {
 	await withTemporaryFixture(cwd('corrupt'), async cwd => {
-		await fs.writeFile(path.join(cwd, 'test.js.snap'), Uint8Array.of(0x00));
+		const snapPath = path.join(cwd, 'test.js.snap');
+		await fs.writeFile(snapPath, Uint8Array.of(0x00));
 		const result = await t.throwsAsync(fixture([], {cwd}));
+		t.log(result.stdout);
 		t.snapshot(result.stats.failed, 'failed tests');
-		t.is([...result.stdout.matchAll(/The snapshot file is corrupted./g)].length, 2);
+		t.is(countMatches(result.stdout, /The snapshot file is corrupted./g), 2);
+		t.is(countMatches(result.stdout, /File path:/g), 2);
+		t.is(
+			countMatches(
+				result.stdout,
+				/Please run AVA again with the .*--update-snapshots.* flag to recreate it\./g
+			),
+			2
+		);
+		t.is(countStringMatches(result.stdout, snapPath), 2);
 	});
 });
 
