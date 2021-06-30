4d3e22038809fec40b2a9ba11a6b2b9d85b58f49
Sam Verschueren <sam.verschueren@gmail.com>
2021-06-30 15:26:16 +0200

    Add StackBlitz example for macros

---
+++ examples/macros/test.js
@@ -0,0 +1,14 @@
+'use strict';
+const test = require('ava');
+
+const {sum} = require('.');
+
+function macro(t, a, b, expected) {
+	t.is(sum(a, b), expected);
+}
+
+macro.title = (providedTitle, a, b, expected) => `${providedTitle || ''} ${a}+${b} = ${expected}`.trim();
+
+test(macro, 2, 2, 4);
+test(macro, 3, 3, 6);
+test('providedTitle', macro, 4, 4, 8);
