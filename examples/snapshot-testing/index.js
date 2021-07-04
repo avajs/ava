ce2e53213f7ae8bc51a06d75e463b2177ddad775
Sam Verschueren <sam.verschueren@gmail.com>
2021-07-04 22:50:36 +0200

    Fix linting

--- examples/snapshot-testing/index.js
+++ examples/snapshot-testing/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 
 const HelloWorld = () => <h1>Hello World...!</h1>;
--- examples/snapshot-testing/test.js
+++ examples/snapshot-testing/test.js
@@ -1,7 +1,9 @@
+/* eslint-disable no-unused-vars */
 import test from 'ava';
 import React from 'react';
 import render from 'react-test-renderer';
-import HelloWorld from '.';
+
+import HelloWorld from './index.js';
 
 test('HelloWorld component', t => {
 	const tree = render.create(<HelloWorld />).toJSON();
