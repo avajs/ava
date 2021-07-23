9a106dd42214e64c5f5f7799f229b4fc37e957eb
Sam Verschueren <sam.verschueren@gmail.com>
2021-07-23 10:23:07 +0200

    Add StackBlitz example endpoint testing

---
+++ examples/endpoint-testing/test.js
@@ -0,0 +1,23 @@
+'use strict';
+const http = require('http');
+
+const test = require('ava');
+const got = require('got');
+const listen = require('test-listen');
+
+const app = require('./app');
+
+test.before(async t => {
+	t.context.server = http.createServer(app);
+	t.context.prefixUrl = await listen(t.context.server);
+});
+
+test.after.always(t => {
+	t.context.server.close();
+});
+
+test.serial('get /user', async t => {
+	const {email} = await got('user', {prefixUrl: t.context.prefixUrl}).json();
+
+	t.is(email, 'ava@rocks.com');
+});
