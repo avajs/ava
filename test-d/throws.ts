83395b737ace4789ad78436dde911ec5c34abddb
Matthew Espino <65783406+mcecode@users.noreply.github.com>
2021-08-17 07:09:51 +0800

    Return undefined when throws assertions fail

--- test-d/throws.ts
+++ test-d/throws.ts
@@ -12,15 +12,15 @@ class CustomError extends Error {
 }
 
 test('throws', t => {
-	expectType<Error | null>(t.throws(() => {}));
-	const error2: CustomError | null = t.throws(() => {});
-	expectType<CustomError | null>(error2);
-	expectType<CustomError | null>(t.throws<CustomError>(() => {}));
+	expectType<Error | undefined>(t.throws(() => {}));
+	const error2: CustomError | undefined = t.throws(() => {});
+	expectType<CustomError | undefined>(error2);
+	expectType<CustomError | undefined>(t.throws<CustomError>(() => {}));
 });
 
 test('throwsAsync', async t => {
-	expectType<Error | null>(await t.throwsAsync(async () => {}));
-	expectType<CustomError | null>(await t.throwsAsync<CustomError>(async () => {}));
-	expectType<Error | null>(await t.throwsAsync(Promise.reject()));
-	expectType<CustomError | null>(await t.throwsAsync<CustomError>(Promise.reject()));
+	expectType<Error | undefined>(await t.throwsAsync(async () => {}));
+	expectType<CustomError | undefined>(await t.throwsAsync<CustomError>(async () => {}));
+	expectType<Error | undefined>(await t.throwsAsync(Promise.reject()));
+	expectType<CustomError | undefined>(await t.throwsAsync<CustomError>(Promise.reject()));
 });
