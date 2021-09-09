aa2b7fa71198a05bf8a24930dbb4bd50167dc24b
Ethan Fann <git@ethanfann.com>
2021-09-09 11:13:10 -0700

    Add tests with inner newline

--- test/trim-off-newlines/test.js
+++ test/trim-off-newlines/test.js
@@ -18,3 +18,21 @@ test('newlines are trimmed on macOS', t => {
 	const desiredText = 'Mac formatted newlines';
 	t.is(trimOffNewlines(text), desiredText);
 });
+
+test('inner newlines are not trimmed on Windows', t => {
+	const text = '\r\nWindows formatted newlines\r\n with an inner newline\r\n'
+	const desiredText = 'Windows formatted newlines\r\n with an inner newline'
+	t.is(trimOffNewlines(text), desiredText);
+})
+
+test('inner newlines are not trimmed on Linux', t => {
+	const text = '\nLinux formatted newlines\n with an inner newline\n'
+	const desiredText = 'Linux formatted newlines\n with an inner newline'
+	t.is(trimOffNewlines(text), desiredText);
+})
+
+test('inner newlines are not trimmed on Mac', t => {
+	const text = '\rMac formatted newlines\r with an inner newline\r'
+	const desiredText = 'Mac formatted newlines\r with an inner newline'
+	t.is(trimOffNewlines(text), desiredText);
+})
