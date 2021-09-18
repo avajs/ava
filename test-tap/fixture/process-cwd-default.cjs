456d5f30f906db006cf0fb92ce5a74967047ccb5
Mark Wubben <mark@novemberborn.net>
2021-09-18 15:21:23 +0200

    Upgrade pkg-conf
    This reverts commit 1d5d65e016343ff0b1b118d4c085b6e82d244e1b.

--- test-tap/fixture/process-cwd-default.cjs
+++ test-tap/fixture/process-cwd-default.cjs
@@ -1,11 +1,10 @@
 const path = require('path');
 
-const pkgConf = require('pkg-conf');
-
 const test = require('../../entrypoints/main.cjs');
 
-test('test', t => {
-	const conf = pkgConf.sync('ava');
-	const pkgDir = path.dirname(pkgConf.filepath(conf));
+test('test', async t => {
+	const {packageConfigSync, packageJsonPath} = await import('pkg-conf'); // eslint-disable-line node/no-unsupported-features/es-syntax
+	const conf = packageConfigSync('ava');
+	const pkgDir = path.dirname(packageJsonPath(conf));
 	t.is(process.cwd(), pkgDir);
 });
