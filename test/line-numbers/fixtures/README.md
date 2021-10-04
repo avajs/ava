7331a1f77c76dfbb440533e45e895ffc7f74756b
Mark Wubben <mark@novemberborn.net>
2021-10-04 22:06:05 +0200

    Add full source map support to line number selection

---
+++ test/line-numbers/fixtures/README.md
@@ -0,0 +1,5 @@
+Compile using:
+
+```console
+npx tsc line-numbers.ts --outDir . --sourceMap --module es2020 --moduleResolution node
+```
--- test/line-numbers/fixtures/line-numbers.js
+++ test/line-numbers/fixtures/line-numbers.js
@@ -1,27 +1,23 @@
 import test from 'ava';
-
-test('unicorn', t => {
-	t.pass();
+test('unicorn', function (t) {
+    t.pass();
 });
-
-test('rainbow', t => {
-	t.pass();
+test('rainbow', function (t) {
+    t.pass();
 });
-
-test.serial('cat', t => {
-	t.pass();
+test.serial('cat', function (t) {
+    t.pass();
 });
-
 test.todo('dog'); // eslint-disable-line ava/no-todo-test
-
 /* eslint-disable max-statements-per-line, ava/no-inline-assertions */
-test('sun', t => t.pass()); test('moon', t => {
-	t.pass();
+test('sun', function (t) { return t.pass(); });
+test('moon', function (t) {
+    t.pass();
 });
 /* eslint-enable max-statements-per-line, ava/no-inline-assertions */
-
-(() => {
-	test('nested call', t => {
-		t.pass();
-	});
+(function () {
+    test('nested call', function (t) {
+        t.pass();
+    });
 })();
+//# sourceMappingURL=line-numbers.js.map
