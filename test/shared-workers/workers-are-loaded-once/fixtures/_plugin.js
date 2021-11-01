a7737cd0a5ea4bd90bfd236e183f75aaabfd39bc
Mark Wubben <mark@novemberborn.net>
2021-11-01 13:15:08 +0100

    Update dependencies
    Also remove it-first dependency.

--- test/shared-workers/workers-are-loaded-once/fixtures/_plugin.js
+++ test/shared-workers/workers-are-loaded-once/fixtures/_plugin.js
@@ -1,9 +1,9 @@
 import * as plugin from 'ava/plugin';
-import itFirst from 'it-first';
 
 const worker = plugin.registerSharedWorker({
 	filename: new URL('_worker.js', import.meta.url),
 	supportedProtocols: ['experimental'],
 });
 
-export const random = itFirst(worker.subscribe());
+const messages = worker.subscribe();
+export const random = messages.next().then(({value}) => value).finally(() => messages.return());
