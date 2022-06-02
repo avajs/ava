191174ea8e2a8a7f31c91ef6ea9e6936a7ea2296
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2022-06-03 04:22:57 +1200

    build: setup build process

--- entrypoints/cli.mjs
+++ entrypoints/cli.mjs
@@ -1,4 +1,3 @@
-#!/usr/bin/env node
 import run from '../lib/cli.js';
 
 run();
---
+++ rollup.config.mjs
@@ -0,0 +1,98 @@
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import {nodeResolve} from '@rollup/plugin-node-resolve';
+import {defineConfig} from 'rollup';
+import autoExternal from 'rollup-plugin-auto-external';
+import dts from 'rollup-plugin-dts';
+
+import pkg from './package.json' assert { "type": "json" };
+
+const getPlugins = () => [autoExternal(), nodeResolve(), commonjs(), json()];
+
+export default defineConfig([
+	{
+		input: './lib/worker/main.cjs',
+
+		output: [
+			{
+				file: pkg.exports['.'].import.default,
+				format: 'module',
+			},
+			{
+				file: pkg.exports['.'].require.default,
+				format: 'commonjs',
+			},
+		],
+
+		plugins: getPlugins(),
+	},
+	{
+		input: './lib/worker/plugin.cjs',
+
+		output: [
+			{
+				file: pkg.exports['./plugin'].import.default,
+				format: 'module',
+			},
+			{
+				file: pkg.exports['./plugin'].require.default,
+				format: 'commonjs',
+			},
+		],
+
+		plugins: getPlugins(),
+	},
+	{
+		input: './entrypoints/eslint-plugin-helper.cjs',
+
+		output: [
+			{
+				file: pkg.exports['./eslint-plugin-helper'],
+				format: 'commonjs',
+			},
+		],
+
+		plugins: getPlugins(),
+	},
+	{
+		input: './entrypoints/cli.mjs',
+
+		output: [
+			{
+				file: pkg.bin.ava,
+				format: 'module',
+				banner: '#!/usr/bin/env node',
+			},
+		],
+
+		plugins: getPlugins(),
+	},
+	{
+		input: './index.d.ts',
+
+		output: [
+			{
+				file: pkg.exports['.'].import.types,
+			},
+			{
+				file: pkg.exports['.'].require.types,
+			},
+		],
+
+		plugins: [dts()],
+	},
+	{
+		input: './plugin.d.ts',
+
+		output: [
+			{
+				file: pkg.exports['./plugin'].import.types,
+			},
+			{
+				file: pkg.exports['./plugin'].require.types,
+			},
+		],
+
+		plugins: [dts()],
+	},
+]);
