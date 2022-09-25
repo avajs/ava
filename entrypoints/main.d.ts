bec96c21bb6d6b453880dc79caa53fad5b1d0c6b
Mark Wubben <mark@novemberborn.net>
2022-09-25 16:52:32 +0200

    Tweak type definitions used in exports map for TS 4.8 compatibility
    Duplicate and export shared types where appropriate, but don't export from the other export file since it seems to confuse things.

--- entrypoints/main.d.ts
+++ entrypoints/main.d.ts
@@ -1,2 +1,12 @@
-export * from './main.cjs';
-export {default} from './main.cjs';
+import type {TestFn} from '../types/test-fn.cjs';
+
+export * from '../types/assertions.cjs';
+export * from '../types/try-fn.cjs';
+export * from '../types/test-fn.cjs';
+export * from '../types/subscribable.cjs';
+
+/** Call to declare a test, or chain to declare hooks or test modifiers */
+declare const test: TestFn;
+
+/** Call to declare a test, or chain to declare hooks or test modifiers */
+export default test;
--- entrypoints/plugin.d.ts
+++ entrypoints/plugin.d.ts
@@ -1,1 +1,6 @@
-export type * from './plugin.cjs';
+import type {SharedWorker} from '../types/shared-worker.cjs';
+
+export function registerSharedWorker<Data = unknown>(options: SharedWorker.Plugin.RegistrationOptions<'ava-4', Data>): SharedWorker.Plugin.Protocol<Data>;
+// Add overloads for additional protocols.
+
+export type {SharedWorker} from '../types/shared-worker.cjs';
