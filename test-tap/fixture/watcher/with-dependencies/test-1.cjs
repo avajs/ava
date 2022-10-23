9cd5ff8466c842a2e4f2fe917ea52fdf50d167fc
Mark Wubben <mark@novemberborn.net>
2022-10-23 15:56:17 +0200

    Require opt-in to AVA 5's watcher and separate install of chokidar
    Restrict @ava/typescript to the ava-3.2 protocol, since the legacy code is not compatible with the ava-6 protocol.
    
    Remove brittle tests for the legacy code.

--- test-tap/fixture/watcher/with-dependencies/test-1.cjs
+++
@@ -1,7 +0,0 @@
-const test = require('../../../../entrypoints/main.cjs');
-
-const dependency = require('./source.cjs');
-
-test('works', t => {
-	t.truthy(dependency);
-});
--- test-tap/fixture/watcher/with-dependencies/test-2.cjs
+++
@@ -1,5 +0,0 @@
-const test = require('../../../../entrypoints/main.cjs');
-
-test('works', t => {
-	t.pass();
-});
