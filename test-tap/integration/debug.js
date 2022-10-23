ac01f3c4abf7dad74d45d79840c0c7bbe9246591
Mark Wubben <mark@novemberborn.net>
2022-10-23 15:56:17 +0200

    Delete brittle tests for watch mode

--- test-tap/integration/debug.js
+++ test-tap/integration/debug.js
@@ -3,7 +3,7 @@ import {test} from 'tap';
 import {execCli} from '../helper/cli.js';
 
 test('bails when using --watch while while debugging', t => {
-	execCli(['debug', '--watch', 'test.cjs'], {dirname: 'fixture/watcher', env: {AVA_FORCE_CI: 'not-ci'}}, (error, stdout, stderr) => {
+	execCli(['debug', '--watch', 'test.cjs'], {dirname: 'fixture/tap', env: {AVA_FORCE_CI: 'not-ci'}}, (error, stdout, stderr) => {
 		t.equal(error.code, 1);
 		t.match(stderr, 'Watch mode is not available when debugging.');
 		t.end();
@@ -11,7 +11,7 @@ test('bails when using --watch while while debugging', t => {
 });
 
 test('bails when debugging in CI', t => {
-	execCli(['debug', 'test.cjs'], {dirname: 'fixture/watcher', env: {AVA_FORCE_CI: 'ci'}}, (error, stdout, stderr) => {
+	execCli(['debug', 'test.cjs'], {dirname: 'fixture/tap', env: {AVA_FORCE_CI: 'ci'}}, (error, stdout, stderr) => {
 		t.equal(error.code, 1);
 		t.match(stderr, 'Debugging is not available in CI.');
 		t.end();
@@ -19,7 +19,7 @@ test('bails when debugging in CI', t => {
 });
 
 test('bails when --tap reporter is used while debugging', t => {
-	execCli(['debug', '--tap', 'test.cjs'], {dirname: 'fixture/watcher', env: {AVA_FORCE_CI: 'not-ci'}}, (error, stdout, stderr) => {
+	execCli(['debug', '--tap', 'test.cjs'], {dirname: 'fixture/tap', env: {AVA_FORCE_CI: 'not-ci'}}, (error, stdout, stderr) => {
 		t.equal(error.code, 1);
 		t.match(stderr, 'The TAP reporter is not available when debugging.');
 		t.end();
