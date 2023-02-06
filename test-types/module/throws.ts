75596139a98d4b4fb4e8a6bba03963fa0904ac23
Tao Cumplido <taocumplido@gmail.com>
2023-02-06 21:20:42 +0100

    Infer thrown error from expectations

--- test-types/module/throws.ts
+++ test-types/module/throws.ts
@@ -12,15 +12,30 @@ class CustomError extends Error {
 }
 
 test('throws', t => {
-	expectType<Error | undefined>(t.throws(() => {}));
+	const error1 = t.throws(() => {});
+	expectType<Error | undefined>(error1);
 	const error2: CustomError | undefined = t.throws(() => {});
 	expectType<CustomError | undefined>(error2);
 	expectType<CustomError | undefined>(t.throws<CustomError>(() => {}));
+	const error3 = t.throws(() => {}, {instanceOf: CustomError});
+	expectType<CustomError | undefined>(error3);
+	const error4 = t.throws(() => {}, {is: new CustomError()});
+	expectType<CustomError | undefined>(error4);
+	const error5 = t.throws(() => {}, {instanceOf: CustomError, is: new CustomError()});
+	expectType<CustomError | undefined>(error5);
 });
 
 test('throwsAsync', async t => {
-	expectType<Error | undefined>(await t.throwsAsync(async () => {}));
+	const error1 = await t.throwsAsync(async () => {});
+	expectType<Error | undefined>(error1);
 	expectType<CustomError | undefined>(await t.throwsAsync<CustomError>(async () => {}));
-	expectType<Error | undefined>(await t.throwsAsync(Promise.reject()));
+	const error2 = await t.throwsAsync(Promise.reject());
+	expectType<Error | undefined>(error2);
 	expectType<CustomError | undefined>(await t.throwsAsync<CustomError>(Promise.reject()));
+	const error3 = await t.throwsAsync(async () => {}, {instanceOf: CustomError});
+	expectType<CustomError | undefined>(error3);
+	const error4 = await t.throwsAsync(async () => {}, {is: new CustomError()});
+	expectType<CustomError | undefined>(error4);
+	const error5 = await t.throwsAsync(async () => {}, {instanceOf: CustomError, is: new CustomError()});
+	expectType<CustomError | undefined>(error5);
 });
