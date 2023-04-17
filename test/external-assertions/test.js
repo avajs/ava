5e52c46a6a6b91b54633bfe7c7edd66fea32a1a4
Irven Aelbrecht <irven.aelbrecht@gmail.com>
2023-04-17 09:22:31 +0200

    :white_check_mark: fix windows test

--- test/external-assertions/test.js
+++ test/external-assertions/test.js
@@ -4,10 +4,10 @@ import {fixture} from '../helpers/exec.js';
 
 test('node assertion ', async t => {
 	const result = await t.throwsAsync(fixture(['assert-failure.js']));
-	t.snapshot(result.stdout.replace(/\r/g, ''));
+	t.snapshot(result.stdout.replace(/\r/g, '').replace(/\/{3}/g, '//'));
 });
 
 test('expect error ', async t => {
 	const result = await t.throwsAsync(fixture(['expect-failure.js']));
-	t.snapshot(result.stdout.replace(/\r/g, ''));
+	t.snapshot(result.stdout.replace(/\r/g, '').replace(/\/{3}/g, '//'));
 });
