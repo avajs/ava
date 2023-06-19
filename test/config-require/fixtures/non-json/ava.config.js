b88c6dffba1bc0561a03c5386a11d9264047ef93
Mark Wubben <mark@novemberborn.net>
2023-06-19 17:22:54 +0200

    Add test for argument serialization

---
+++ test/config-require/fixtures/non-json/ava.config.js
@@ -0,0 +1,5 @@
+export default {
+	require: [
+		['./required.mjs', new Map([['hello', 'world']])],
+	],
+};
--- test/config-require/test.js
+++ test/config-require/test.js
@@ -7,6 +7,16 @@ test('loads required modules with arguments', async t => {
 	t.is(result.stats.passed.length, 2);
 });
 
+test('non-JSON arguments can be provided (worker threads)', async t => {
+	const result = await fixture([], {cwd: cwd('non-json')});
+	t.is(result.stats.passed.length, 1);
+});
+
+test.failing('non-JSON arguments can be provided (child process)', async t => {
+	const result = await fixture(['--no-worker-threads'], {cwd: cwd('non-json')});
+	t.is(result.stats.passed.length, 1);
+});
+
 test('loads required modules, not as an array', async t => {
 	const result = await fixture([], {cwd: cwd('single-argument')});
 	t.is(result.stats.passed.length, 1);
