7b0c55b183a76e18f1ffdccec6febb3c8a9194d8
Max Isom <hi@maxisom.me>
2023-09-25 12:31:01 -0700

    Gate behind nonSemVerExperiments

--- test/internal-events/fixtures/ava.config.js
+++ test/internal-events/fixtures/ava.config.js
@@ -6,6 +6,9 @@ export default {
 	files: [
 		'test.js',
 	],
+	nonSemVerExperiments: {
+		observeRunsFromConfig: true,
+	},
 	async observeRun(run) {
 		for await (const event of run.events) {
 			internalEvents.push(event);
