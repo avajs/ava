78f9d371f99b45a9633cb629b4bac8862ed2dbed
Max Isom <hi@maxisom.me>
2023-09-25 14:54:45 -0700

    Add types

---
+++ entrypoints/internal.d.ts
@@ -0,0 +1,25 @@
+import type {StateChangeEvent} from '../types/state-change-events.d.cts';
+
+export type RunEvent = {
+	type: 'stateChange';
+	stateChange: StateChangeEvent;
+} | {
+	type: 'run';
+	plan: {
+		bailWithoutReporting: boolean;
+		debug: boolean;
+		failFastEnabled: boolean;
+		filePathPrefix: string;
+		files: string[];
+		matching: boolean;
+		previousFailures: number;
+		runOnlyExclusive: boolean;
+		firstRun: boolean;
+	};
+};
+
+export type {StateChangeEvent} from '../types/state-change-events.d.cts';
+
+export type Run = {
+	events: AsyncIterableIterator<RunEvent>;
+};
