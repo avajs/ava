a3da70a010c4e003ce7e0940d3daf51f6bb2534a
Mark Wubben <mark@novemberborn.net>
2023-12-30 20:37:53 +0100

    Remove code guards against obsolete API usage

--- test/multiple-implementations/test.js
+++ test/multiple-implementations/test.js
@@ -4,10 +4,10 @@ import {fixture} from '../helpers/exec.js';
 
 test('test()', async t => {
 	const result = await t.throwsAsync(fixture(['test.js']));
-	t.regex(result.stdout, /AVA 4 no longer supports multiple implementations/);
+	t.regex(result.stdout, /Expected an implementation/);
 });
 
 test('t.try()', async t => {
 	const result = await t.throwsAsync(fixture(['try.js']));
-	t.regex(result.stdout, /AVA 4 no longer supports t\.try\(\) with multiple implementations/);
+	t.regex(result.stdout, /Expected an implementation/);
 });
