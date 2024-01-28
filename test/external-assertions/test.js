30d61d3bb6290c006a419a6fcea1438c1672d265
Mark Wubben <mark@novemberborn.net>
2024-01-28 21:05:36 +0100

    Fix external-assertions snapshot for Node.js 20.11
    Vary the snapshots by minor version were appropriate.

--- test/external-assertions/test.js
+++ test/external-assertions/test.js
@@ -13,21 +13,24 @@ const snapshotStdout = (t, stdout) => {
 	t.snapshot(normalized);
 };
 
-const major = process.versions.node.split('.')[0];
-
-for (const version of ['18', '20', '21']) {
+for (const [label, selector] of Object.entries({
+	'^18': /^18\./,
+	'^20 < 20.11': /^20\.(\d\.|10\.)/,
+	'^20.11': /^20\.(1[1-9]\.|[2-9]\d\.)/,
+	'^21': /^21\./,
+})) {
 	// Tests need to be declared for all versions, so that snapshots can be
 	// updated by running `npx test-ava -u test/external-assertions/test.js` for
 	// each supported version. However only the tests for the current version
 	// can run, so skip the others.
-	const declare = version === major ? test : test.skip;
+	const declare = selector.test(process.versions.node) ? test : test.skip;
 
-	declare(`node assertion (node.js v${version})`, async t => {
+	declare(`node assertion (node.js v${label})`, async t => {
 		const result = await t.throwsAsync(fixture(['assert-failure.js']));
 		snapshotStdout(t, result.stdout);
 	});
 
-	declare(`expect error (node.js v${version})`, async t => {
+	declare(`expect error (node.js v${label})`, async t => {
 		const result = await t.throwsAsync(fixture(['expect-failure.js']));
 		snapshotStdout(t, result.stdout);
 	});
