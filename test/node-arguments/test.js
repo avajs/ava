3566d40ae98857fa3a5a715219a705869888ab75
Bogdan Chadkin <opensource@trysound.io>
2024-12-29 20:37:23 +0700

    Upgrade args-tokenizer

--- test/node-arguments/test.js
+++ test/node-arguments/test.js
@@ -1,6 +1,6 @@
 import test from '@ava/test';
 
-import {cwd, fixture} from '../helpers/exec.js';
+import {cleanOutput, cwd, fixture} from '../helpers/exec.js';
 
 test('passed node arguments to workers', async t => {
 	const options = {
@@ -33,14 +33,14 @@ test('`filterNodeArgumentsForWorkerThreads` configuration ignored for worker pro
 	t.snapshot(result.stats.passed, 'tests pass');
 });
 
-test('forgive incomplete --node-arguments', async t => {
+test('detects incomplete --node-arguments', async t => {
 	const options = {
 		cwd: cwd('node-arguments'),
 	};
 
 	const result = await t.throwsAsync(fixture(['--node-arguments="--foo=\'bar"', 'node-arguments.js'], options));
 
-	t.snapshot(result.stats.passed, 'tests pass');
+	t.snapshot(cleanOutput(result.stderr), 'fails with message');
 });
 
 test('reads node arguments from config', async t => {
