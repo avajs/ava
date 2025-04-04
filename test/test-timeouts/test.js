0b81b8d5ceda36de62150274fab63bebf61c76dd
Matthew Douglass <5410142+mdouglass@users.noreply.github.com>
2025-04-04 07:09:20 -0700

    move test to idle-timeouts directory

--- test/test-timeouts/test.js
+++ test/test-timeouts/test.js
@@ -14,9 +14,3 @@ test('timeout messages must be strings', async t => {
 	t.snapshot(error.message, 'error message');
 	t.snapshot(error.formattedDetails, 'formatted details');
 });
-
-test('timeouts are not blocked by console output', async t => {
-	const result = await t.throwsAsync(fixture(['console-output.js']));
-	const error = result.stats.getError(result.stats.failed[0]);
-	t.snapshot(error.message, 'timeout despite console output');
-});
