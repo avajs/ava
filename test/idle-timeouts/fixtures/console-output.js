03cd818ee9c55830fc08d2a3bfc456ef429f4dd5
Mark Wubben <mark@novemberborn.net>
2025-04-15 21:48:55 +0200

    Clean up test
    * Set a short timeout so it finishes more quickly
    * Remove the snapshot; the assertion message was the same as the error message being snapshotted
    * Tweak delays within test to be clearer that it can run for a second, but then we keep writing to stdout/stderr
    * Use timers/promises rather than new Promise()
    * Alternate writing to stdout and stderr

--- test/idle-timeouts/fixtures/console-output.js
+++ test/idle-timeouts/fixtures/console-output.js
@@ -1,9 +1,13 @@
+import {setTimeout as delay} from 'node:timers/promises';
 import test from 'ava';
 
 test('timeout with console output', async t => {
-	t.timeout(5, 'timeout despite console output');
-	for (;;) {
-		console.log('sorry for the noise');
-		await new Promise(resolve => setTimeout(resolve, 1000));
+	t.timeout(1000, 'timeout despite console output');
+	for (let i = 0; await delay(100, true); i++) {
+		if (i % 2 === 0) {
+			console.log('stdout');
+		} else {
+			console.error('stderr');
+		}
 	}
 });
