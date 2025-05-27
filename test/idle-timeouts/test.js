f4a52bf67ec84bb1b084e344150f3a5f2d93cb3f
Michael Mulet <mike@lfds.io>
2025-05-27 14:45:42 -0500

    not sure why this failed in CI. It works on my macos node 18.20

--- test/idle-timeouts/test.js
+++ test/idle-timeouts/test.js
@@ -5,5 +5,5 @@ import {fixture} from '../helpers/exec.js';
 test('idle timeouts are not blocked by console output', async t => {
 	const result = await t.throwsAsync(fixture(['console-output.js']));
 	const error = result.stats.getError(result.stats.failed[0]);
-	t.is(error.message, 'timeout despite console output');
+	t.is(error && error.message, 'timeout despite console output');
 });
