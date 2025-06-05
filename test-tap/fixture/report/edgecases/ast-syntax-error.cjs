57ba5f36691dde368817589671ffe73da002dcac
donnnq <gemervin14@gmail.com>
2025-06-06 07:51:27 +0800

    Update ast-syntax-error.cjs

--- test-tap/fixture/report/edgecases/ast-syntax-error.cjs
+++ test-tap/fixture/report/edgecases/ast-syntax-error.cjs
@@ -1,7 +1,9 @@
 const test = require('../../../../entrypoints/main.cjs');
 
-const fn = do {
-  t => t.pass();
+// Define the test function directly
+const fn = t => {
+  t.pass();
 };
 
+// Use the test function in the test definition
 test('fn', fn);
