78e37ce1cec96f28d2b5bace95248999e6878244
Mark Wubben <mark@novemberborn.net>
2025-06-06 22:52:53 +0200

    Remove TypeScript loader example
    It's a fast-moving space with a lot of complicated failure modes. I'd rather not recommend anything other than @ava/typescript.

--- examples/typescript-loader/index.ts
+++
@@ -1,1 +0,0 @@
-export const multiply = (a: number, b: number) => a * b;
--- examples/typescript-loader/test.ts
+++
@@ -1,7 +0,0 @@
-import test from 'ava';
-import {multiply} from './index.js';
-
-test('multiply', t => {
-	t.is(multiply(1, 0), 0);
-	t.is(multiply(2, 3), 6);
-});
