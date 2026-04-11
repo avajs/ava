d44ae7b3681dbb4bbb80590586c36f57c8b1097d
Mark Wubben <mark@novemberborn.net>
2026-04-11 20:18:11 +0200

    ava/plugin isn't meant to have a default export

--- entrypoints/plugin.js
+++ entrypoints/plugin.js
@@ -1,8 +1,1 @@
-import {registerSharedWorker} from '../lib/worker/plugin.js';
-
-const plugin = {
-	registerSharedWorker,
-};
-
-export default plugin;
 export {registerSharedWorker} from '../lib/worker/plugin.js';
