b5424bb745b5274d8b883c054dd7498becc6bbdd
Mark Wubben <mark@novemberborn.net>
2026-04-27 22:05:11 +0200

    Use new runIf condition

--- test/external-assertions/test.js
+++ test/external-assertions/test.js
@@ -14,7 +14,6 @@ const snapshotStdout = (t, stdout) => {
 };
 
 for (const [label, selector] of Object.entries({
-	'^20.19': /^20\.(19\.|[2-9]\d\.)/,
 	'^22.20': /^22\.(2\d\.|[3-9]\d\.)/,
 	'^24.12': /^24\.(1[2-9]\.|[2-9]\d\.)/,
 	'^25': /^25\./,
@@ -23,14 +22,14 @@ for (const [label, selector] of Object.entries({
 	// updated by running `npx test-ava -u test/external-assertions/test.js` for
 	// each supported version. However only the tests for the current version
 	// can run, so skip the others.
-	const declare = selector.test(process.versions.node) ? test : test.skip;
+	const run = selector.test(process.versions.node);
 
-	declare(`node assertion (node.js v${label})`, async t => {
+	test.runIf(run)(`node assertion (${label})`, async t => {
 		const result = await t.throwsAsync(fixture(['assert-failure.js']));
 		snapshotStdout(t, result.stdout);
 	});
 
-	declare(`expect error (node.js v${label})`, async t => {
+	test.runIf(run)(`expect error (${label})`, async t => {
 		const result = await t.throwsAsync(fixture(['expect-failure.js']));
 		snapshotStdout(t, result.stdout);
 	});
