52cade41e315d5f4f9b6ccf77b277ef2e7941a39
Ramiro Aguilera <raguilera321@gmail.com>
2026-07-09 22:25:28 -0700

    Change quotes from double to single in test

--- examples/watch-mode/basic.js
+++ examples/watch-mode/basic.js
@@ -2,7 +2,7 @@ import test from "ava";
 
 // Run this example with: `npx ava --watch examples/watch-mode/basic.js`
 
-test("live reload", t => {
+test('live reload', t => {
 	const value = 2 + 2;
 	t.is(value, 4);
 });
