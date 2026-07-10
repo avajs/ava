a71f75e7327f1d2b9408a2a6d546aa5f768c75fe
Romie321 <raguilera321@gmail.com>
2026-07-09 20:06:17 -0700

    Fix formatting in watch mode example

--- examples/watch-mode/basic.js
+++ examples/watch-mode/basic.js
@@ -1,8 +1,8 @@
 import test from "ava";
 
-//Run this example with: `npx ava --watch examples/watch-mode/basic.js`
+// Run this example with: `npx ava --watch examples/watch-mode/basic.js`
 
-test("live reload", t => {
+test("live reload", (t) => {
 	const value = 2 + 2;
 	t.is(value, 4);
 });
