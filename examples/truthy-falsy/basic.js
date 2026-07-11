eb4852a557e68d2230e736f14edb4eac5be080e7
Ramiro Aguilera <raguilera321@gmail.com>
2026-07-10 18:18:36 -0700

    Fix string quotes in basic.js test example

--- examples/truthy-falsy/basic.js
+++ examples/truthy-falsy/basic.js
@@ -2,7 +2,7 @@ import test from 'ava';
 
 //	This example shows how to use the truthy and falsy assertions in AVA.
 
-test("truthy and falsy values", t => {
+test('truthy and falsy values', t => {
 	t.truthy('hello world');
 	t.falsy('');
 });
