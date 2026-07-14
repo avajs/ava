e3573d10896597dc1d8b4a237def3e03b7d15275
Romie321 <raguilera321@gmail.com>
2026-07-14 11:59:18 -0700

    Add example for using watch mode with AVA

--- examples/watch-mode/test.js
+++ examples/watch-mode/test.js
@@ -1,6 +1,6 @@
 import test from "ava";
 
-// Run this example with: `npx ava --watch examples/watch-mode/basic.js`
+// Run this example with: `npx ava --watch examples/watch-mode/test.js`
 
 test("live reload", (t) => {
 	const value = 2 + 2;
